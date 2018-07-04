b49e30362472f414631935478978553ab8ff5f2f
Eddie Cai <eddie.cai.linux@gmail.com>
2018-07-04 12:50:24 +0800

    common: build: add more function and command
    move some command to function. so we can call from outside
    
    Change-Id: Iab2f4017c76a7f1e57c3cf072bd349247d328814
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- common/build.sh
+++ common/build.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-source BoardConfig.mk
-echo "============================================"
-echo "TARGET_ARCH=$ARCH"
-echo "TARGET_PLATFORM=$TARGET_PRODUCT"
-echo "TARGET_UBOOT_CONFIG=$UBOOT_DEFCONFIG"
-echo "TARGET_KERNEL_CONFIG=$KERNEL_DEFCONFIG"
-echo "TARGET_BUILDROOT_CONFIG=$CFG_BUILDROOT"
-echo "TARGET_RECOVERY_CONFIG=$CFG_RECOVERY"
-echo "TARGET_PCBA_CONFIG=$CFG_PCBA"
-echo "============================================"
+COMMON_DIR=$(cd `dirname $0`; pwd)
+if [ -h $0 ]
+then
+        CMD=$(readlink $0)
+        COMMON_DIR=$(dirname $CMD)
+fi
+cd $COMMON_DIR
+cd ../../..
+TOP_DIR=$(pwd)
+COMMON_DIR=$TOP_DIR/device/rockchip/common
+BOARD_CONFIG=$TOP_DIR/device/rockchip/.BoardConfig.mk
+source $BOARD_CONFIG
 
 if [ ! -n "$1" ];then
-	echo "build all as default"
-	BUILD_TARGET=all
+	echo "build all and save all as default"
+	BUILD_TARGET=allsave
 else
-	echo "build $1 only"
 	BUILD_TARGET="$1"
 fi
 
@@ -24,17 +25,22 @@ usage()
 	echo "====USAGE: build.sh modules===="
 	echo "uboot              -build uboot"
 	echo "kernel             -build kernel"
-	echo "rootfs             -build buildroot rootfs"
-	echo "yocto              -build yocto rootfs"
+	echo "rootfs             -build default rootfs, currently build buildroot as default"
+	echo "buildroot          -build buildroot rootfs"
+	echo "yocto              -build yocto rootfs, currently build ros as default"
+	echo "ros                -build ros rootfs"
 	echo "debian             -build debian rootfs"
 	echo "pcba               -build pcba"
+	echo "all                -build uboot, kernel, rootfs, recovery image"
 	echo "default            -build all modules"
 }
 
 function build_uboot(){
 	# build uboot
-	echo "====Start build uboot===="
-	cd u-boot && make distclean && ./make.sh $UBOOT_DEFCONFIG && cd -
+	echo "============Start build uboot============"
+	echo "TARGET_UBOOT_CONFIG=$UBOOT_DEFCONFIG"
+	echo "========================================="
+	cd u-boot && ./make.sh $UBOOT_DEFCONFIG && cd -
 	if [ $? -eq 0 ]; then
 		echo "====Build uboot ok!===="
 	else
@@ -45,8 +51,12 @@ function build_uboot(){
 
 function build_kernel(){
 	# build kernel
-	echo "====Start build kernel===="
-	cd kernel && make ARCH=$ARCH distclean && make ARCH=$ARCH $KERNEL_DEFCONFIG && make ARCH=$ARCH $KERNEL_DTS.img -j$JOBS && cd -
+	echo "============Start build kernel============"
+	echo "TARGET_ARCH          =$ARCH"
+	echo "TARGET_KERNEL_CONFIG =$KERNEL_DEFCONFIG"
+	echo "TARGET_KERNEL_DTS    =$KERNEL_DTS"
+	echo "=========================================="
+	cd $TOP_DIR/kernel && make ARCH=$ARCH $KERNEL_DEFCONFIG && make ARCH=$ARCH $KERNEL_DTS.img -j$JOBS && cd -
 	if [ $? -eq 0 ]; then
 		echo "====Build kernel ok!===="
 	else
@@ -55,10 +65,12 @@ function build_kernel(){
 	fi
 }
 
-function build_rootfs(){
+function build_buildroot(){
 	# build buildroot
-	echo "====Start build buildroot===="
-	./device/rockchip/$TARGET_PRODUCT/mk-buildroot.sh
+	echo "==========Start build buildroot=========="
+	echo "TARGET_BUILDROOT_CONFIG=$CFG_BUILDROOT"
+	echo "========================================="
+	$COMMON_DIR/mk-buildroot.sh $BOARD_CONFIG
 	if [ $? -eq 0 ]; then
 		echo "====Build buildroot ok!===="
 	else
@@ -67,22 +79,46 @@ function build_rootfs(){
 	fi
 }
 
-function build_yocto(){
-	# build yocto
-	echo "====Start build yocto===="
-	./device/rockchip/$TARGET_PRODUCT/mk-yocto.sh
+function build_rootfs(){
+	build_buildroot
+}
+
+function build_ros(){
+	# build ros
+	echo "======Start build yocto======"
+	echo "YOCTO_MACHINE=$YOCTO_MACHINE"
+	echo "============================="
+	$COMMON_DIR/mk-ros.sh $BOARD_CONFIG
 	if [ $? -eq 0 ]; then
-		echo "====Build yocto ok!===="
+		echo "====Build ros ok!===="
 	else
-		echo "====Build yocto failed!===="
+		echo "====Build ros failed!===="
 		exit 1
 	fi
 }
 
+function build_yocto(){
+	build_ros
+}
+
+function build_debian(){
+        # build debian
+        echo "====Start build debian===="
+	$COMMON_DIR/mk-debian.sh
+        if [ $? -eq 0 ]; then
+                echo "====Build debian ok!===="
+        else
+                echo "====Build debian failed!===="
+                exit 1
+        fi
+}
+
 function build_recovery(){
 	# build recovery
-	echo "====Start build recovery===="
-	./device/rockchip/$TARGET_PRODUCT/mk-recovery.sh
+	echo "==========Start build recovery=========="
+	echo "TARGET_RECOVERY_CONFIG=$CFG_RECOVERY"
+	echo "========================================"
+	$COMMON_DIR/mk-recovery.sh $BOARD_CONFIG
 	if [ $? -eq 0 ]; then
 		echo "====Build recovery ok!===="
 	else
@@ -93,8 +129,10 @@ function build_recovery(){
 
 function build_pcba(){
 	# build pcba
-	echo "====Start build recovery===="
-	./device/rockchip/$TARGET_PRODUCT/mk-pcba.sh
+	echo "==========Start build pcba=========="
+	echo "TARGET_PCBA_CONFIG=$CFG_PCBA"
+	echo "===================================="
+	$COMMON_DIR/mk-pcba.sh $BOARD_CONFIG
 	if [ $? -eq 0 ]; then
 		echo "====Build pcba ok!===="
 	else
@@ -104,23 +142,77 @@ function build_pcba(){
 }
 
 function build_all(){
+	echo "============================================"
+	echo "TARGET_ARCH=$ARCH"
+	echo "TARGET_PLATFORM=$TARGET_PRODUCT"
+	echo "TARGET_UBOOT_CONFIG=$UBOOT_DEFCONFIG"
+	echo "TARGET_KERNEL_CONFIG=$KERNEL_DEFCONFIG"
+	echo "TARGET_BUILDROOT_CONFIG=$CFG_BUILDROOT"
+	echo "TARGET_RECOVERY_CONFIG=$CFG_RECOVERY"
+	echo "TARGET_PCBA_CONFIG=$CFG_PCBA"
+	echo "============================================"
+	# clean uboot and kernel before build all
+	#cd $TOP_DIR/u-boot/ && make distclean && cd -
+	#cd $TOP_DIR/kernel && make distclean && cd -
 	build_uboot
 	build_kernel
 	build_rootfs
 	build_recovery
 }
 
-TOP_DIR=$(pwd)
-source buildroot/build/envsetup.sh $CFG_BUILDROOT
-TARGET_PRODUCT=`get_target_board_type $CFG_BUILDROOT`
-PACK_TOOL_DIR=tools/linux/Linux_Pack_Firmware
-IMAGE_PATH=rockdev/Image-$TARGET_PRODUCT
-DATE=$(date  +%Y%m%d.%H%M)
-STUB_PATH=Image/"$KERNEL_DTS"_"$DATE"_RELEASE_TEST
-STUB_PATH="$(echo $STUB_PATH | tr '[:lower:]' '[:upper:]')"
-export STUB_PATH=$TOP_DIR/$STUB_PATH
-export STUB_PATCH_PATH=$STUB_PATH/PATCHES
+function build_firmware(){
+	# mkfirmware.sh to genarate image
+	./mkfirmware.sh $BOARD_CONFIG
+	if [ $? -eq 0 ]; then
+	    echo "Make image ok!"
+	else
+	    echo "Make image failed!"
+	    exit 1
+	fi
+}
 
+function build_updateimg(){
+	IMAGE_PATH=$TOP_DIR/rockdev/Image-$TARGET_PRODUCT
+	PACK_TOOL_DIR=$TOP_DIR/tools/linux/Linux_Pack_Firmware
+	cp -f $IMAGE_PATH/* $PACK_TOOL_DIR/rockdev/Image/
+
+	echo "Make update.img"
+	cd $PACK_TOOL_DIR/rockdev && ./mkupdate.sh && cd -
+	if [ $? -eq 0 ]; then
+	   echo "Make update image ok!"
+	else
+	   echo "Make update image failed!"
+	   exit 1
+	fi
+	mv $PACK_TOOL_DIR/rockdev/update.img $IMAGE_PATH/
+}
+
+function build_save(){
+	IMAGE_PATH=$TOP_DIR/rockdev/Image-$TARGET_PRODUCT
+	DATE=$(date  +%Y%m%d.%H%M)
+	STUB_PATH=Image/"$KERNEL_DTS"_"$DATE"_RELEASE_TEST
+	STUB_PATH="$(echo $STUB_PATH | tr '[:lower:]' '[:upper:]')"
+	export STUB_PATH=$TOP_DIR/$STUB_PATH
+	export STUB_PATCH_PATH=$STUB_PATH/PATCHES
+	mkdir -p $STUB_PATH
+
+	#Generate patches
+	$TOP_DIR/.repo/repo/repo forall -c "$TOP_DIR/device/rockchip/common/gen_patches_body.sh"
+
+	#Copy stubs
+	$TOP_DIR/.repo/repo/repo manifest -r -o $STUB_PATH/manifest_${DATE}.xml
+	mkdir -p $STUB_PATCH_PATH/kernel
+	cp $TOP_DIR/kernel/.config $STUB_PATCH_PATH/kernel
+	cp $TOP_DIR/kernel/vmlinux $STUB_PATCH_PATH/kernel
+	mkdir -p $STUB_PATH/IMAGES/
+	cp $IMAGE_PATH/* $STUB_PATH/IMAGES/
+
+	#Save build command info
+	echo "UBOOT:  defconfig: $UBOOT_DEFCONFIG" >> $STUB_PATH/build_cmd_info
+	echo "KERNEL: defconfig: $KERNEL_DEFCONFIG, dts: $KERNEL_DTS" >> $STUB_PATH/build_cmd_info
+	echo "BUILDROOT: $CFG_BUILDROOT" >> $STUB_PATH/build_cmd_info
+
+}
 #=========================
 # build target
 #=========================
@@ -133,6 +225,9 @@ elif [ $BUILD_TARGET == kernel ];then
 elif [ $BUILD_TARGET == rootfs ];then
     build_rootfs
     exit 0
+elif [ $BUILD_TARGET == buildroot ];then
+    build_buildroot
+    exit 0
 elif [ $BUILD_TARGET == recovery ];then
     build_recovery
     exit 0
@@ -142,58 +237,35 @@ elif [ $BUILD_TARGET == pcba ];then
 elif [ $BUILD_TARGET == yocto ];then
     build_yocto
     exit 0
-elif [ $BUILD_TARGET != all ];then
+elif [ $BUILD_TARGET == ros ];then
+    build_ros
+    exit 0
+elif [ $BUILD_TARGET == debian ];then
+    build_debian
+    exit 0
+elif [ $BUILD_TARGET == updateimg ];then
+    build_updateimg
+    exit 0
+elif [ $BUILD_TARGET == all ];then
+    build_all
+    exit 0
+elif [ $BUILD_TARGET == firmware ];then
+    build_firmware
+    exit 0
+elif [ $BUILD_TARGET == save ];then
+    build_save
+    exit 0
+elif [ $BUILD_TARGET != allsave ];then
 	echo "Can't found build config, please check again"
 	usage
 	exit 1
 fi
 
-#==========================
-# default build all modules
-#==========================
+
+#============================================================
+# default build all modules, make all image and save stuff
+#============================================================
 build_all
-
-# mkfirmware.sh to genarate image
-echo "make and copy images"
-./mkfirmware.sh $CFG_BUILDROOT
-if [ $? -eq 0 ]; then
-    echo "Make image ok!"
-else
-    echo "Make image failed!"
-    exit 1
-fi
-
-mkdir -p $PACK_TOOL_DIR/rockdev/Image/
-cp -f $IMAGE_PATH/* $PACK_TOOL_DIR/rockdev/Image/
-
-echo "Make update.img"
-cd $PACK_TOOL_DIR/rockdev && ./mkupdate.sh
-if [ $? -eq 0 ]; then
-   echo "Make update image ok!"
-else
-   echo "Make update image failed!"
-   exit 1
-fi
-cd -
-
-mv $PACK_TOOL_DIR/rockdev/update.img $IMAGE_PATH/
-rm $PACK_TOOL_DIR/rockdev/Image -rf
-
-mkdir -p $STUB_PATH
-
-#Generate patches
-.repo/repo/repo forall -c "$TOP_DIR/device/rockchip/common/gen_patches_body.sh"
-
-#Copy stubs
-.repo/repo/repo manifest -r -o $STUB_PATH/manifest_${DATE}.xml
-
-mkdir -p $STUB_PATCH_PATH/kernel
-cp kernel/.config $STUB_PATCH_PATH/kernel
-cp kernel/vmlinux $STUB_PATCH_PATH/kernel
-mkdir -p $STUB_PATH/IMAGES/
-cp $IMAGE_PATH/* $STUB_PATH/IMAGES/
-
-#Save build command info
-echo "UBOOT:  defconfig: $UBOOT_DEFCONFIG" >> $STUB_PATH/build_cmd_info
-echo "KERNEL: defconfig: $KERNEL_DEFCONFIG, dts: $KERNEL_DTS" >> $STUB_PATH/build_cmd_info
-echo "BUILDROOT: $LUNCH" >> $STUB_PATH/build_cmd_info
+build_firmware
+build_updateimg
+build_save
