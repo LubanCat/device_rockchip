fb6aa128f13eb9fa111463861c36e2371b65814c
Eddie Cai <eddie.cai.linux@gmail.com>
2018-07-06 09:01:23 +0800

    rk3326: add BoardConfig.mk
    Change-Id: I50e9ff44424f70005e56633cd00b19bc567b05e8
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

---
+++ rk3326/BoardConfig.mk
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#=========================
+# Compile Config
+#=========================
+# Target arch
+ARCH=arm64
+# Uboot defconfig
+UBOOT_DEFCONFIG=evb-rk3326
+# Kernel defconfig
+KERNEL_DEFCONFIG=rk3326_linux_defconfig
+# Kernel dts
+KERNEL_DTS=rk3326-evb-linux-lp3-v10
+# Buildroot config
+CFG_BUILDROOT=rockchip_rk3326
+# Recovery config
+CFG_RECOVERY=rockchip_rk3326_recovery
+# Pcba config
+CFG_PCBA=rockchip_rk3326_pcba
+# Build jobs
+JOBS=12
+# Yocto machine
+YOCTO_MACHINE=rockchip-rk3326-evb
+#=========================
+# Platform Target
+#=========================
+TARGET_PRODUCT=rk3326
+
+# Set rootfs type, see buildroot.
+# ext4 squashfs
+ROOTFS_TYPE=ext4
+
+# Set data partition type.
+# ext2 squashfs
+OEM_PARTITION_TYPE=ext2
+
+# Set flash type.
+# support <emmc, nand, spi_nand, spi_nor>
+FLASH_TYPE=emmc
+
+#OEM config: /oem/dueros/aispeech/iflytekSDK/CaeDemo_VAD/smart_voice
+OEM_PATH=oem
