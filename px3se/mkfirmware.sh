92e81385d92cac403e783e6027f6c0050dcfc313
Eddie Cai <eddie.cai.linux@gmail.com>
2018-07-18 16:49:04 +0800

    px3se: modify mkfirmware, remove unused file
    Change-Id: I6b289cda2aad263d311df1a07806311c9e6cc947
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- px3se/mkfirmware.sh
+++ px3se/mkfirmware.sh
@@ -1,120 +1,135 @@
-TOOL_PATH=$(pwd)/build
-IMAGE_OUT_PATH=$(pwd)/rockdev
-KERNEL_PATH=$(pwd)/kernel
-UBOOT_PATH=$(pwd)/u-boot
-ROOTFS_PATH=$(pwd)/rootfs
-DEVICE_IMG_PATH=$(pwd)/device/rockchip/px3se/rockdev
-PARAMETER_PATH=$DEVICE_IMG_PATH/parameter.txt
-OEM_IMG_PATH=$DEVICE_IMG_PATH/oem.img
-USER_DATA_IMG_PATH=$DEVICE_IMG_PATH/userdata.img
-MISC_IMG_PATH=$DEVICE_IMG_PATH/wipe_all-misc.img
-ROOTFS_IMG_PATH=$(pwd)/buildroot/output/rockchip_px3se/images/rootfs.ext4
-RECOVERY_PATH=$(pwd)/buildroot/output/rockchip_px3se_recovery/images/recovery.img
-TRUST_PATH=$UBOOT_PATH/trust.img
-BOOT_PATH=$KERNEL_PATH/zboot.img
-LOADER_PATH=$UBOOT_PATH/*_loader_*.bin
+#!/bin/bash
 
-mkdir -p $IMAGE_OUT_PATH
-
-if [ -f $ROOTFS_IMG_PATH ]
+DEVICE_DIR=$(cd `dirname $0`; pwd)
+if [ -h $0 ]
 then
-	echo -n "create rootfs.img..."
-	ln -s -f $ROOTFS_IMG_PATH $IMAGE_OUT_PATH/rootfs.img
-	echo "done"
+        CMD=$(readlink $0)
+        DEVICE_DIR=$(dirname $CMD)
+fi
+cd $DEVICE_DIR
+cd ../../..
+TOP_DIR=$(pwd)
+
+source $TOP_DIR/device/rockchip/.BoardConfig.mk
+ROCKDEV=$TOP_DIR/rockdev
+ROOTFS_PATH=$(pwd)/rootfs
+PARAMETER_DEBIAN=$DEVICE_DIR/parameter.txt
+PARAMETER_BUILDROOT=$DEVICE_DIR/parameter.txt
+OEM_DIR=$TOP_DIR/device/rockchip/oem/oem_normal
+USER_DATA_DIR=$TOP_DIR/device/rockchip/userdata/userdata_normal
+MISC_IMG=$TOP_DIR/device/rockchip/rockimg/wipe_all-misc.img
+BUILDROOT_IMG=$TOP_DIR/buildroot/output/$CFG_BUILDROOT/images/rootfs.ext4
+RECOVERY_IMG=$TOP_DIR/buildroot/output/$CFG_RECOVERY/images/recovery.img
+TRUST_IMG=$TOP_DIR/u-boot/trust_emmc.img
+UBOOT_IMG=$TOP_DIR/u-boot/uboot.img
+BOOT_IMG=$TOP_DIR/kernel/boot.img
+LOADER=$TOP_DIR/u-boot/*_loader_v*.bin
+SPINOR_LOADER=$TOP_DIR/u-boot/*_loader_spinor_v*.bin
+MKOEM=$TOP_DIR/device/rockchip/common/mk-oem.sh
+MKUSERDATA=$TOP_DIR/device/rockchip/common/mk-userdata.sh
+ROOTFS_TYPE=
+mkdir -p $ROCKDEV
+if [ ! -n "$1" ]
+then
+echo "build buildroot type rootfs as default"
+ROOTFS_TYPE=buildroot
 else
-	echo -e "\e[31m error: $ROOTFS_IMG_PATH not found! \e[0m"
-	exit 0
+ROOTFS_TYPE="$1"
 fi
 
-if [ -f $RECOVERY_PATH ]
+if [ $ROOTFS_TYPE = debian ]
+then
+	echo -n "create rootfs.img and parameter.txt..."
+	ln -s -f $ROOTFS_PATH/linaro-rootfs.img $ROCKDEV/rootfs.img
+	ln -s -f $PARAMETER $ROCKDEV/parameter.txt
+	echo "done."
+else
+	echo -n "create rootfs.img and parameter.txt..."
+	ln -s -f $BUILDROOT_IMG $ROCKDEV/rootfs.img
+	ln -s -f $PARAMETER_BUILDROOT $ROCKDEV/parameter.txt
+	echo "done"
+fi
+
+if [ -f $RECOVERY_IMG ]
 then
 	echo -n "create recovery.img..."
-	ln -s -f $RECOVERY_PATH $IMAGE_OUT_PATH/
+	ln -s -f $RECOVERY_IMG $ROCKDEV/recovery.img
 	echo "done."
 else
-	echo -e "\e[31m error: $RECOVERY_PATH not found! \e[0m"
-	exit 0
+	echo -e "\e[31m error: $RECOVERY_IMG not found! \e[0m"
 fi
 
-if [ -f $MISC_IMG_PATH ]
+if [ -f $MISC_IMG ]
 then
 	echo -n "create misc.img..."
-	ln -s -f $MISC_IMG_PATH $IMAGE_OUT_PATH/misc.img
+	ln -s -f $MISC_IMG $ROCKDEV/misc.img
 	echo "done."
 else
-	echo -e "\e[31m error: $MISC_IMG_PATH not found! \e[0m"
-	exit 0
+	echo -e "\e[31m error: $MISC_IMG not found! \e[0m"
 fi
 
-if [ -f $PARAMETER_PATH ]
-then
-	echo -n "create parameter.txt..."
-	ln -s -f $PARAMETER_PATH $IMAGE_OUT_PATH/
-	echo "done."
-else
-	echo -e "\e[31m error: $PARAMETER_PATH not found! \e[0m"
-	exit 0
-fi
-
-if [ -f $OEM_IMG_PATH ]
+if [ -d $OEM_DIR ]
 then
 	echo -n "create oem.img..."
-	ln -s -f $OEM_IMG_PATH $IMAGE_OUT_PATH/
+	$MKOEM $OEM_DIR $ROCKDEV/oem.img ext2
 	echo "done."
 else
-	echo -e "\e[31m error: $OEM_IMG_PATH not found! \e[0m"
-	exit 0
+	echo -e "\e[31m error: create oem image fail! \e[0m"
 fi
 
-if [ -f $USER_DATA_IMG_PATH ]
+if [ -d $USER_DATA_DIR ]
 then
 	echo -n "create userdata.img..."
-	ln -s -f $USER_DATA_IMG_PATH $IMAGE_OUT_PATH/
+	$MKUSERDATA $USER_DATA_DIR $ROCKDEV/userdata.img fat
 	echo "done."
 else
-	echo -e "\e[31m error: $USER_DATA_IMG_PATH not found! \e[0m"
-	exit 0
+	echo -e "\e[31m error: $USER_DATA_DIR not found! \e[0m"
 fi
 
-if [ -f $UBOOT_PATH/uboot.img ]
+if [ -f $UBOOT_IMG ]
 then
         echo -n "create uboot.img..."
-        ln -s -f $UBOOT_PATH/uboot.img $IMAGE_OUT_PATH/uboot.img
+        ln -s -f $UBOOT_IMG $ROCKDEV/uboot.img
         echo "done."
 else
-        echo -e "\e[31m error: $UBOOT_PATH/uboot.img not found! Please make it from $UBOOT_PATH first! \e[0m"
-	exit 0
+        echo -e "\e[31m error: $UBOOT_IMG not found! \e[0m"
 fi
 
-if [ -f $TRUST_PATH ]
+if [ -f $TRUST_IMG ]
 then
         echo -n "create trust.img..."
-        ln -s -f $TRUST_PATH $IMAGE_OUT_PATH/trust.img
+        ln -s -f $TRUST_IMG $ROCKDEV/trust.img
         echo "done."
 else
-        echo -e "\e[31m error: $UBOOT_PATH/trust.img not found! Please make it from $UBOOT_PATH first! \e[0m"
-	exit 0
+        echo -e "\e[31m error: $TRUST_IMG not found! \e[0m"
 fi
 
-if [ -f $LOADER_PATH ]
+if [ -f $LOADER ]
 then
         echo -n "create loader..."
-        ln -s -f $LOADER_PATH $IMAGE_OUT_PATH/MiniLoaderAll.bin
+        ln -s -f $LOADER $ROCKDEV/MiniLoaderAll.bin
         echo "done."
 else
-	echo -e "\e[31m error: $UBOOT_PATH/*loader_*.bin not found,or there are multiple loaders! Please make it from $UBOOT_PATH first! \e[0m"
-	rm $LOADER_PATH
-	exit 0
+	echo -e "\e[31m error: $LOADER not found,or there are multiple loaders! \e[0m"
+	rm $LOADER
+fi
+
+if [ -f $SPINOR_LOADER ]
+then
+        echo -n "create spinor loader..."
+        ln -s -f $SPINOR_LOADER $ROCKDEV/MiniLoaderAll_SpiNor.bin
+        echo "done."
+else
+	rm $SPINOR_LOADER_PATH 2>/dev/null
 fi
 
 if [ -f $BOOT_PATH ]
 then
 	echo -n "create boot.img..."
-	ln -s -f $BOOT_PATH $IMAGE_OUT_PATH/boot.img
+	ln -s -f $BOOT_IMG $ROCKDEV/boot.img
 	echo "done."
 else
-	echo -e "\e[31m error: $KERNEL_PATH/boot.img not found! \e[0m"
-	exit 0
+	echo -e "\e[31m error: $BOOT_IMG not found! \e[0m"
 fi
 
 echo -e "\e[36m Image: image in rockdev is ready \e[0m"
