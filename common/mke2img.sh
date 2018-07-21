cfb48d25009a3f17161437a3b0db978f59ef3481
Hans Yang <yhx@rock-chips.com>
2018-07-21 10:51:52 +0800

    mke2img.sh: fix folder size calculation error when it have multilevel subdirectory
    Change-Id: If7c9ad04fce7dc4d711864dcc2b29875c74fdaef
    Signed-off-by: Hans Yang <yhx@rock-chips.com>

--- common/mke2img.sh
+++ common/mke2img.sh
@@ -2,7 +2,7 @@
 
 SRC=$1
 DST=$2
-SIZE=$(du -h -BM --max-depth=1 $SRC | awk '{print int($1)}')
+SIZE=$(du -h -BM --max-depth=0 $SRC | awk '{print int($1)}')
 # echo "create image size=${SIZE}M"
 dd if=/dev/zero of=$DST bs=1M count=$SIZE >/dev/null 2>&1
 mke2fs -F -t ext2 $DST >/dev/null 2>&1
