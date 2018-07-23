b44752164ee23062a634d6081d0014fa325ca47b
Eddie Cai <eddie.cai.linux@gmail.com>
2018-07-23 08:36:36 +0800

    common: mke2img.sh: fix pack image error
    Change-Id: Ib1cccd1dd4d6d275021c7b4c901a5b8be5828128
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- common/mke2img.sh
+++ common/mke2img.sh
@@ -2,8 +2,9 @@
 
 SRC=$1
 DST=$2
-SIZE=$(du -h -BM --max-depth=0 $SRC | awk '{print int($1)}')
+SIZE=$(du -h -BM -s $SRC | awk '{print int($1)}')
+SIZE=`expr $SIZE + 1`
 # echo "create image size=${SIZE}M"
-dd if=/dev/zero of=$DST bs=1M count=$SIZE >/dev/null 2>&1
+dd if=/dev/zero of=$DST bs=1M count=$SIZE>/dev/null 2>&1
 mke2fs -F -t ext2 $DST >/dev/null 2>&1
 mke2fs -F -d $SRC $DST >/dev/null 2>&1
