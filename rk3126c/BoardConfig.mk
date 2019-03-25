1fe92938924a9684f2a3309d391e1eef0f23d494
Hans Yang <yhx@rock-chips.com>
2019-03-25 16:46:23 +0800

    rk3126c: first init for BoardConfig and parameter
    Change-Id: I2ccc1d9a9c6f302475161e6e9f894e00107da805
    Signed-off-by: Hans Yang <yhx@rock-chips.com>

---
+++ rk3126c/BoardConfig.mk
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Target arch
+export RK_ARCH=arm
+# Uboot defconfig
+export RK_UBOOT_DEFCONFIG=rk3126
+# Kernel defconfig
+export RK_KERNEL_DEFCONFIG=rockchip_linux_defconfig
+# Kernel dts
+export RK_KERNEL_DTS=rk3126-linux
+# boot image type
+export RK_BOOT_IMG=zboot.img
+# kernel image path
+export RK_KERNEL_IMG=kernel/arch/arm/boot/zImage
+# parameter for GPT table
+export RK_PARAMETER=parameter-buildroot.txt
+# Buildroot config
+export RK_CFG_BUILDROOT=rockchip_rk3126c
+# Recovery config
+export RK_CFG_RECOVERY=rockchip_rk3126c_recovery
+# ramboot config
+export RK_CFG_RAMBOOT=
+# Pcba config
+export RK_CFG_PCBA=rockchip_rk3126c_pcba
+# Build jobs
+export RK_JOBS=12
+# target chip
+export RK_TARGET_PRODUCT=px3se
+# Set rootfs type, including ext2 ext4 squashfs
+export RK_ROOTFS_TYPE=ext4
+# rootfs image path
+export RK_ROOTFS_IMG=buildroot/output/$RK_CFG_BUILDROOT/images/rootfs.$RK_ROOTFS_TYPE
+# Set oem partition type, including ext2 squashfs
+export RK_OEM_FS_TYPE=ext2
+# Set userdata partition type, including ext2, fat
+export RK_USERDATA_FS_TYPE=ext2
+# Set flash type. support <emmc, nand, spi_nand, spi_nor>
+export RK_STORAGE_TYPE=emmc
+#OEM config
+export RK_OEM_DIR=oem_normal
+#userdata config
+export RK_USERDATA_DIR=userdata_normal
+#misc image
+export RK_MISC=wipe_all-misc.img
+#choose enable distro module
+export RK_DISTRO_MODULE=
