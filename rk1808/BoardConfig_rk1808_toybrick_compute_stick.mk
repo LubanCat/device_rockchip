ef9d4da1458eef18b900d9ce54be8a37ca8fce94
Lin Huang <hl@rock-chips.com>
2019-05-06 19:53:07 +0800

    rk1808: fix rk1808_toybrick_compute_stick.mk buildroot config errot
    Change-Id: If2721a9dc2b3d0c3babd57d1d32c57cd0cbf4d8d
    Signed-off-by: Lin Huang <hl@rock-chips.com>

--- rk1808/BoardConfig_rk1808_toybrick_compute_stick.mk
+++ rk1808/BoardConfig_rk1808_toybrick_compute_stick.mk
@@ -15,7 +15,7 @@ export RK_KERNEL_IMG=kernel/arch/arm64/boot/Image
 # parameter for GPT table
 export RK_PARAMETER=parameter-buildroot.txt
 # Buildroot config
-export RK_CFG_BUILDROOT=rockchip_rk1808
+export RK_CFG_BUILDROOT=rockchip_rk1808_compute_stick
 # Recovery config
 export RK_CFG_RECOVERY=rockchip_rk1808_recovery
 # ramboot config
