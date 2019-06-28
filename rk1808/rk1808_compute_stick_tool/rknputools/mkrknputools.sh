312b66ca4965c299fc2c0ea7281eac37a993e950
Lin Jianhua <linjh@rock-chips.com>
2019-06-28 14:24:45 +0800

    rk1808: update the script files of rknputools
    Change-Id: I1455300415f1a24c7b2f0ed93c3dede95b63bb14
    Signed-off-by: Lin Jianhua <linjh@rock-chips.com>

--- rk1808/rk1808_compute_stick_tool/rknputools/mkrknputools.sh
+++ rk1808/rk1808_compute_stick_tool/rknputools/mkrknputools.sh
@@ -7,16 +7,20 @@ TOP_DIR=$(realpath $SCRIPT_DIR/../../../../..)
 cd $TOP_DIR
 
 source $TOP_DIR/device/rockchip/.BoardConfig.mk
-TOOLS_OUT_DIR=$TOP_DIR/device/rockchip/rk1808/rk1808_compute_stick_tool/rknputools/build/
+BUILD_DIR=build
+TOOLS_OUT_DIR=$TOP_DIR/device/rockchip/rk1808/rk1808_compute_stick_tool/rknputools/$BUILD_DIR
 LIB_OUT_DIR=$TOOLS_OUT_DIR/lib64
 
 RKNPUTOOLS_DIR=$TOP_DIR/external/RKNPUTools
 RKNN_API_DIR=$RKNPUTOOLS_DIR/rknn-api/$RK_RKNN_API_PLT
 RKNN_SDK_DIR=$RKNN_API_DIR/rknn_api_sdk
-RKNN_MOBILENET=$RKNN_SDK_DIR/build/rknn_mobilenet
+RKNN_MOBILENET=$RKNN_SDK_DIR/$BUILD_DIR/rknn_mobilenet
+RKNN_SSD=$RKNN_SDK_DIR/$BUILD_DIR/rknn_ssd
 NPU_TRANSFER_PROXY=$RKNPUTOOLS_DIR/npu_transfer_proxy/$RK_NPU_TRANSFER_PROXY_ARCH/npu_transfer_proxy
 
+
 if [ -d "$TOOLS_OUT_DIR" ]; then
+    echo "delete dir $BUILD_DIR"
     rm -rf $TOOLS_OUT_DIR
 fi
 mkdir -p $LIB_OUT_DIR
@@ -28,13 +32,22 @@ if [ ! -d "$TARGET_OUTPUT_DIR" ]; then
 fi
 
 if [ -d $RKNN_SDK_DIR ]; then
-    echo -n "compile rknn api sdk..."
     cd $RKNN_SDK_DIR
-    if [ -d $RKNN_SDK_DIR/build ]; then
-        rm -rf build
+    if [ -d $BUILD_DIR ]; then
+        rm -rf $BUILD_DIR
     fi
-    mkdir build; cd build; cmake ..; make
+    mkdir $BUILD_DIR; cd $BUILD_DIR
 
+    echo -n "compile $RK_RKNN_API_ARCH rknn api..."
+    if [ "$RK_RKNN_API_ARCH" == "arm" ]
+    then
+        cmake -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++ ..
+        make
+    else
+        cmake ..; make
+    fi
+
+    echo "copy $RK_RKNN_API_PLT images and resource to $BUILD_DIR"
     if [ -f $RKNN_MOBILENET ]
     then
         echo -n "copy rknn_mobilenet..."
@@ -43,11 +56,20 @@ if [ -d $RKNN_SDK_DIR ]; then
     else
         echo "warning: $RKNN_MOBILENET not found!"
     fi
+
+    if [ -f $RKNN_SSD ]
+    then
+        echo -n "copy rknn_ssd..."
+        cp $RKNN_SSD $TOOLS_OUT_DIR/rknn_ssd
+        echo "done."
+    else
+        echo "warning: $RKNN_SSD not found!"
+    fi
 fi
 
 if [ -f $NPU_TRANSFER_PROXY ]
 then
-    echo -n "copy npu_transfer_proxy..."
+    echo -n "copy $RK_NPU_TRANSFER_PROXY_ARCH npu_transfer_proxy..."
     cp $NPU_TRANSFER_PROXY $TOOLS_OUT_DIR/npu_transfer_proxy
     echo "done."
 else
@@ -58,8 +80,8 @@ if [ -d $RKNN_API_DIR ]
 then
     echo -n "copy libs and resource..."
     cp $RKNN_API_DIR/tmp/* $TOOLS_OUT_DIR/
-    cp $RKNN_API_DIR/rknn_api_sdk/rknn_api/lib64/* $LIB_OUT_DIR/
-    cp $RKNN_API_DIR/rknn_api_sdk/3rdparty/opencv/lib64/* $LIB_OUT_DIR/
+    cp $RKNN_SDK_DIR/rknn_api/$RK_RKNN_API_ARCH/lib64/* $LIB_OUT_DIR/
+    cp $RKNN_SDK_DIR/3rdparty/opencv/$RK_RKNN_API_ARCH/lib64/* $LIB_OUT_DIR/
     echo "done."
 else
     echo -e "\e[31m error: $RKNN_API_DIR not found! \e[0m"
