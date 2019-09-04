ee793e25d146052f8d52b5de29a610596a5831cd
vicent.chi <vicent.chi@rock-chips.com>
2019-09-04 09:54:53 +0000

    device: rv1108: Add more comments and different module distinctions
    Change-Id: I4d140d9098bb9b4afbc732f1d67ce49a7488b2e1
    Signed-off-by: vicent.chi <vicent.chi@rock-chips.com>

--- rv1108/BoardConfig-lock-evb-v10.mk
+++ rv1108/BoardConfig-lock-evb-v10.mk
@@ -1,40 +1,79 @@
 #!/bin/bash
 
+########################################
+#              Chip Info               #
+########################################
 # Target arch
 export RK_ARCH=arm
 # target chip
 export RK_TARGET_PRODUCT=rv1108
+
+########################################
+#             Board Info               #
+########################################
 #Target Board Version
 export RK_TARGET_BOARD_VERSION=lock-evb-v10
-# Buildroot config
-export RK_CFG_BUILDROOT=rockchip_rv1108_lock_defconfig
-# Kernel defconfig
-export RK_KERNEL_DEFCONFIG=rv1108-${RK_TARGET_BOARD_VERSION}_defconfig
-# Kernel dts
-export RK_KERNEL_DTS=rv1108-${RK_TARGET_BOARD_VERSION}
-# kernel image path
-export RK_KERNEL_IMG=kernel/arch/arm/boot/Image
-# setting.ini for firmware
-export RK_SETTING_INI=setting-emmc-root.ini
-# Build jobs
-export RK_JOBS=12
-# Set rootfs type, including ext2 ext4 squashfs
-export RK_ROOTFS_TYPE=cpio.lz4
-# rootfs image path
-export RK_ROOTFS_IMG=rockdev/rootfs.${RK_ROOTFS_TYPE}
 # Set flash type. support <emmc, nand, nor>
 export RK_STORAGE_TYPE=emmc
-# Set userdata config
-export RK_USERDATA_FILESYSTEM_TYPE=ext4
-export RK_USERDATA_FILESYSTEM_SIZE=32M
-# Set root data config
-export RK_ROOT_FILESYSTEM_TYPE=ext4
-export RK_ROOT_FILESYSTEM_SIZE=32M
+
+########################################
+#           Buildroot Config           #
+########################################
+# Buildroot config
+export RK_CFG_BUILDROOT=rockchip_rv1108_lock_defconfig
+
+########################################
+#            Rootfs Config             #
+########################################
+# Set rootfs type, including ext2 ext4 squashfs
+export RK_ROOTFS_TYPE=cpio.lz4
+
+########################################
+#            Loader Config             #
+########################################
 # Set loader config
 export RK_LOADER_POWER_HOLD_GPIO_GROUP=3
 export RK_LOADER_POWER_HOLD_GPIO_INDEX=12
 export RK_LOADER_EMMC_TURNING_DEGREE=0
 export RK_LOADER_BOOTPART_SELECT=0
+
+########################################
+#            Kernel Config             #
+########################################
+# Kernel defconfig
+export RK_KERNEL_DEFCONFIG=rv1108-${RK_TARGET_BOARD_VERSION}_defconfig
+# Kernel dts
+export RK_KERNEL_DTS=rv1108-${RK_TARGET_BOARD_VERSION}
+
+########################################
+#           Userdata Config            #
+########################################
+# Set userdata config
+export RK_USERDATA_FILESYSTEM_TYPE=ext4
+export RK_USERDATA_FILESYSTEM_SIZE=32M
+
+########################################
+#             Root Config              #
+########################################
+# Set root data config
+export RK_ROOT_FILESYSTEM_TYPE=ext4
+export RK_ROOT_FILESYSTEM_SIZE=32M
+
+########################################
+#            Firmware Config           #
+########################################
+# setting.ini for firmware
+export RK_SETTING_INI=setting-emmc.ini
+
+########################################
+#            Build Config              #
+########################################
+# Build jobs
+export RK_JOBS=12
+
+########################################
+#              APP Config              #
+########################################
 # Set ui_resolution
 export RK_UI_RESOLUTION=360x640
 # Set first start application
