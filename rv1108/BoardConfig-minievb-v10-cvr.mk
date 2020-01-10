ad160b2c1f308ad9f8d1a091d5aee17acf82722f
Zhichao Yu <zhichao.yu@rock-chips.com>
2020-01-10 15:57:58 +0800

    rv1108/cvr: enable device manager in system_manager
    Change-Id: I247dadf35a7e81ed352ed674ede17c4aa206a94e
    Signed-off-by: Zhichao Yu <zhichao.yu@rock-chips.com>

--- rv1108/BoardConfig-minievb-v10-cvr.mk
+++ rv1108/BoardConfig-minievb-v10-cvr.mk
@@ -91,4 +91,4 @@ export RK_FACE_FOV_SCALE_FACTOR_Y=1
 # Set UVC source
 export RK_UVC_USE_SL_MODULE=n
 # Set first start application
-export RK_FIRST_START_APP="cvr udevd udevadm"
+export RK_FIRST_START_APP="cvr system_manager device_manager"
