2ae53bcebafdcb2e38c030f53e3c33e3a77ed76c
Ziyuan Xu <xzy.xu@rock-chips.com>
2020-02-27 16:13:51 +0800

    rk3326: declare a config for debian 10
    Change-Id: Ia09abbaa046f1b6d5185c2eb0b188528993fa7cc
    Signed-off-by: Ziyuan Xu <xzy.xu@rock-chips.com>

--- rk3326/BoardConfig.mk
+++ rk3326/BoardConfig.mk
@@ -19,6 +19,8 @@ export RK_KERNEL_ZIMG=kernel/arch/arm64/boot/Image.lz4
 export RK_PARAMETER=parameter.txt
 # Buildroot config
 export RK_CFG_BUILDROOT=rockchip_rk3326_64
+# Debian 10 config
+export RK_DISTRO_DEFCONFIG=rk3326_defconfig
 # Recovery config
 export RK_CFG_RECOVERY=rockchip_rk3326_recovery
 # ramboot config
