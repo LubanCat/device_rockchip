c055a1a00fcb0672aaa6e88251a921b8c4633b10
Weiwen Chen <cww@rock-chips.com>
2020-03-05 10:26:32 +0800

    oem: ipc: remove mediaserver conf to /oem
    Change-Id: I269388426e44e7aeacfee099ab659b3d3cd5c1d4
    Signed-off-by: Weiwen Chen <cww@rock-chips.com>

--- oem/oem_ipc/RkLunch.sh
+++ oem/oem_ipc/RkLunch.sh
@@ -9,4 +9,8 @@ mkdir -p /data/video1
 mkdir -p /data/photo0
 mkdir -p /data/photo1
 
-mediaserver  -c /usr/share/mediaserver/camerax2_audio_capture_nv12_pcm_enc_h264_mp2_muxer_mp4_rtsp_rtmp.conf -d &
+if [ -f $HOME/usr/share/mediaserver/mediaserver.conf  ]; then
+    mediaserver -c $HOME/usr/share/mediaserver/mediaserver.conf -d &
+else
+    mediaserver -c /usr/share/mediaserver/mediaserver.conf -d &
+fi
