ad4da35d1fc9ef512c4bc8ef9d53eb57ae3f2695
Caesar Wang <wxt@rock-chips.com>
2020-03-05 11:04:21 +0800

    device/rockchip: rk3399pro: used the v14 by default
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: I75b0d1f266b081f41c67678f681045d0a869cfb1

--- rk3399pro/BoardConfig_debian.mk
+++ rk3399pro/BoardConfig_debian.mk
@@ -7,7 +7,7 @@ export RK_UBOOT_DEFCONFIG=rk3399pro
 # Kernel defconfig
 export RK_KERNEL_DEFCONFIG=rockchip_linux_defconfig
 # Kernel dts
-export RK_KERNEL_DTS=rk3399pro-evb-v11-linux
+export RK_KERNEL_DTS=rk3399pro-evb-v14-linux
 # boot image type
 export RK_BOOT_IMG=boot.img
 # kernel image path
@@ -15,7 +15,7 @@ export RK_KERNEL_IMG=kernel/arch/arm64/boot/Image
 # parameter for GPT table
 export RK_PARAMETER=parameter.txt
 # Buildroot config
-export RK_CFG_BUILDROOT=rockchip_rk3399pro
+export RK_CFG_BUILDROOT=rockchip_rk3399pro_combine
 # Recovery config
 export RK_CFG_RECOVERY=rockchip_rk3399pro_recovery
 # ramboot config
@@ -28,8 +28,10 @@ export RK_JOBS=12
 export RK_TARGET_PRODUCT=rk3399pro
 # Set rootfs type, including ext2 ext4 squashfs
 export RK_ROOTFS_TYPE=ext4
+# yocto machine
+export RK_YOCTO_MACHINE=rockchip-rk3399pro-evb
 # rootfs image path
-export RK_ROOTFS_IMG=rockdev/rootfs.${RK_ROOTFS_TYPE}
+export RK_ROOTFS_IMG=debian/linaro-rootfs.img
 # Set oem partition type, including ext2 squashfs
 export RK_OEM_FS_TYPE=ext2
 # Set userdata partition type, including ext2, fat
