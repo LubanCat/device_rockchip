728c207408a4e657199e7d4309dba00e76260cf4
Caesar Wang <wxt@rock-chips.com>
2020-03-16 09:18:53 +0800

    device/rockchip: used the rk3399 EVB IND board by default
    Change-Id: Ic97af388924194a7cdf341eadad71b7b23dae320
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- rk3399/BoardConfig.mk
+++ rk3399/BoardConfig.mk
@@ -7,7 +7,7 @@ export RK_UBOOT_DEFCONFIG=rk3399
 # Kernel defconfig
 export RK_KERNEL_DEFCONFIG=rockchip_linux_defconfig
 # Kernel dts
-export RK_KERNEL_DTS=rk3399-sapphire-excavator-linux
+export RK_KERNEL_DTS=rk3399-evb-ind-lpddr4-linux
 # boot image type
 export RK_BOOT_IMG=boot.img
 # kernel image path
