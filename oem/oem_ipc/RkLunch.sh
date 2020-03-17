78e6bf6c309648a216fca93ae8bba89423470b66
Fenrir Lin <fenrir.lin@rock-chips.com>
2020-03-17 17:19:55 +0800

    oem_ipc: update prebuilt web applicantion for v0.2.3
    deleted: /www/cgi-bin/entry.cgi
    
    Signed-off-by: Fenrir Lin <fenrir.lin@rock-chips.com>
    Change-Id: I39476edc05347c90c85718763858e5e0744c43c1

--- oem/oem_ipc/RkLunch.sh
+++ oem/oem_ipc/RkLunch.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 #
 
+check_linker()
+{
+	[ ! -L "$2" ] && ln -sf $1 $2
+}
+
+check_linker /userdata   /oem/www/userdata
+check_linker /userdata   /oem/www/userdata
+check_linker /media/usb0 /oem/www/usb0
+check_linker /mnt/sdcard /oem/www/sdcard
+
 dbserver &
 storage_manager &
 netserver &
-mkdir -p /data/video0
-mkdir -p /data/video1
-mkdir -p /data/photo0
-mkdir -p /data/photo1
 
 if [ -f $HOME/usr/share/mediaserver/mediaserver.conf  ]; then
-    mediaserver -c $HOME/usr/share/mediaserver/mediaserver.conf -d &
+    mediaserver -c $HOME/usr/share/mediaserver/mediaserver.conf &
 else
-    mediaserver -c /usr/share/mediaserver/mediaserver.conf -d &
+    mediaserver -c /usr/share/mediaserver/mediaserver.conf &
 fi
