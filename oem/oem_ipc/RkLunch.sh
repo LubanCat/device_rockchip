d0d8195793383f98c06820efa24811ff8d518f39
Weiwen Chen <cww@rock-chips.com>
2020-03-20 11:53:00 +0800

    use ipc-daemon run
    Change-Id: I1cc61c399227e15533a155807a56ace92dd79a61
    Signed-off-by: Weiwen Chen <cww@rock-chips.com>

--- oem/oem_ipc/RkLunch.sh
+++ oem/oem_ipc/RkLunch.sh
@@ -11,12 +11,5 @@ check_linker /userdata   /oem/www/userdata
 check_linker /media/usb0 /oem/www/usb0
 check_linker /mnt/sdcard /oem/www/sdcard
 
-dbserver &
-storage_manager &
-netserver &
+ipc-daemon &
 
-if [ -f $HOME/usr/share/mediaserver/mediaserver.conf  ]; then
-    mediaserver -c $HOME/usr/share/mediaserver/mediaserver.conf &
-else
-    mediaserver -c /usr/share/mediaserver/mediaserver.conf &
-fi
