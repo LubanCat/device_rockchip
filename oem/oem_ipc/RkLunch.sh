ea10c0f1b6f9aa6958e8a3a58dbed14aeb2f6c91
Yu YongZhen <yuyz@rock-chips.com>
2020-04-07 10:52:52 +0800

    oem_ipc: add ispp init script
    This CL should be reverted if the v4l2 ctrls and links
    are correctly set in mediaserver/rkmedia app
    
    Change-Id: Id27ab0ba272c33e98619abfc72309cc4be7fa181
    Signed-off-by: Yu YongZhen <yuyz@rock-chips.com>
    Signed-off-by: Cody Xie <cody.xie@rock-chips.com>

--- oem/oem_ipc/RkLunch.sh
+++ oem/oem_ipc/RkLunch.sh
@@ -11,5 +11,36 @@ check_linker /userdata   /oem/www/userdata
 check_linker /media/usb0 /oem/www/usb0
 check_linker /mnt/sdcard /oem/www/sdcard
 
-ipc-daemon &
+#vpu 600M
+echo 600 >/sys/kernel/debug/mpp_service/rkvenc/clk_core
 
+#cpu x2
+echo 0 > /sys/devices/system/cpu/cpu2/online
+echo 0 > /sys/devices/system/cpu/cpu3/online
+
+#npu 600M
+echo 600000000 > /sys/kernel/debug/clk/clk_core_npu/clk_rate
+echo 600000000 > /sys/kernel/debug/clk/aclk_npu/clk_rate
+
+dbserver &
+storage_manager &
+netserver &
+
+ls /sys/class/drm | grep "card0-"
+if [ $? -ne 0 ] ;then
+  echo "not found display"
+  HasDisplay=0
+else
+  echo "find display"
+  HasDisplay=1
+fi
+
+if [ $HasDisplay -eq 1 ]; then
+  sh /oem/isppx4_init.sh
+  sleep 2
+  mediaserver -c /oem/usr/share/mediaserver/rv1109/camerax4_audio_g711a_rga_mp4_rtsp_rtmp_jpeg_face_display_v2.conf &
+else
+  sh /oem/isppx3_init.sh
+  sleep 2
+  mediaserver -c /oem/usr/share/mediaserver/rv1109/camerax3_audio_g711a_rga_mp4_rtsp_rtmp_jpeg_face_v2.conf &
+fi
---
+++ oem/oem_ipc/isppx3_init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# dmesg | egrep 'isp|camera|5695|v4l2'
+
+#enable scal0
+media-ctl -d /dev/media1 -l '"rkispp-subdev":2->"rkispp_scale0":0[1]'
+#enable scal1
+media-ctl -d /dev/media1 -l '"rkispp-subdev":2->"rkispp_scale1":0[1]'
+#enable scal2
+#media-ctl -d /dev/media1 -l '"rkispp-subdev":2->"rkispp_scale2":0[1]'
+
+# media-ctl -d /dev/media0 --set-v4l2 '"rkisp-isp-subdev":0[crop:(0,0)/2560x1440]'
+# media-ctl -d /dev/media0 --set-v4l2 '"rkisp-isp-subdev":2[crop:(0,0)/2560x1440]'
+
+v4l2-ctl -d /dev/video13  --set-fmt-video=width=2688,height=1520,pixelformat=NV12 &
+v4l2-ctl -d /dev/video14  --set-fmt-video=width=2688,height=1520,pixelformat=NV12 &
+v4l2-ctl -d /dev/video15  --set-fmt-video=width=1280,height=720,pixelformat=NV12  &
+#v4l2-ctl -d /dev/video16  --set-fmt-video=width=1280,height=720,pixelformat=NV12 --stream-mmap=4 --stream-count=10 --stream-poll &
