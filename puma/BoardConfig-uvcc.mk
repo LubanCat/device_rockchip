46f587288e2ef103403ec70be9cfa5f5f678865a
huangjc <huangjc@rock-chips.com>
2020-04-27 12:45:13 +0800

    puma: add BoardConfig-uvcc.mk
    Signed-off-by:huangjc <huangjc@rock-chips.com>
    Change-Id: I5582c2904e19d56b803189660f95e0ceeab4ed36

---
+++ puma/BoardConfig-uvcc.mk
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Target arch
+export RK_ARCH=arm
+# Uboot defconfig
+export RK_UBOOT_DEFCONFIG=rv1126
+# Uboot image format type: fit(flattened image tree)
+export RK_UBOOT_FORMAT_TYPE=fit
+# Kernel defconfig
+export RK_KERNEL_DEFCONFIG=rv1109_defconfig
+# Kernel defconfig fragment
+export RK_KERNEL_DEFCONFIG_FRAGMENT=
+# Kernel dts
+export RK_KERNEL_DTS=rv1126-evb-ddr3-v10
+# boot image type
+export RK_BOOT_IMG=zboot.img
+# kernel image path
+export RK_KERNEL_IMG=kernel/arch/arm/boot/zImage
+# parameter for GPT table
+export RK_PARAMETER=parameter-buildroot-fit.txt
+# Buildroot config
+export RK_CFG_BUILDROOT=rockchip_puma_uvcc
+# Recovery config
+export RK_CFG_RECOVERY=rockchip_puma_recovery
+# ramboot config
+export RK_CFG_RAMBOOT=
+# Pcba config
+export RK_CFG_PCBA=
+# Build jobs
+export RK_JOBS=12
+# target chip
+export RK_TARGET_PRODUCT=puma
+# Set rootfs type, including ext2 ext4 squashfs
+export RK_ROOTFS_TYPE=ext4
+# rootfs image path
+export RK_ROOTFS_IMG=rockdev/rootfs.${RK_ROOTFS_TYPE}
+# Set ramboot image type
+export RK_RAMBOOT_TYPE=
+# Set oem partition type, including ext2 squashfs
+export RK_OEM_FS_TYPE=ext2
+# Set userdata partition type, including ext2, fat
+export RK_USERDATA_FS_TYPE=ext2
+#OEM config
+export RK_OEM_DIR=oem_uvcc
+#userdata config
+export RK_USERDATA_DIR=userdata_normal
+#misc image
+export RK_MISC=wipe_all-misc.img
+#choose enable distro module
+export RK_DISTRO_MODULE=
