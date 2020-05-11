b3c1b0fcd48769ef2069fee752d9b2897d7f131b
huangjc <huangjc@rock-chips.com>
2020-05-11 14:46:14 +0800

    oem_uvcc:update RkLunch-stop.sh
    Change-Id: I22303897d4593814195df35cd664266a7041fa78
    Signed-off-by: huangjc <huangjc@rock-chips.com>

--- oem/oem_uvcc/RkLunch-stop.sh
+++ oem/oem_uvcc/RkLunch-stop.sh
@@ -2,10 +2,8 @@
 #
 killall aicamera.sh
 killall eptz.sh
-killall rk_npu_uvc_device
+killall eptz_demo
 killall smart_display_service
 killall uvc_app
 killall mediaserver
-killall storage_manager
-killall dbserver
 echo "Stop Application ..."
