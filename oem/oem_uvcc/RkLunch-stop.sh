1b3992342fa2f80495831c1a7c42788a77c38e9e
huangjc <huangjc@rock-chips.com>
2020-05-14 11:08:26 +0800

    oem_uvcc:fix killall fail sometimes
    Change-Id: Id0b6fdaf18b941bf17b076cb6551746d61c98d83
    Signed-off-by: huangjc <huangjc@rock-chips.com>

--- oem/oem_uvcc/RkLunch-stop.sh
+++ oem/oem_uvcc/RkLunch-stop.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-killall aicamera.sh
-killall eptz.sh
-killall eptz_demo
-killall smart_display_service
-killall uvc_app
-killall mediaserver
+killall -9 aicamera.sh
+killall -9 eptz.sh
+killall -9 eptz_demo
+killall -9 smart_display_service
+killall -9 uvc_app
+killall -9 mediaserver
 echo "Stop Application ..."
