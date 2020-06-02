89e877cfa0dd76d0572a204b83a767cf90a2b57f
huangjc <huangjc@rock-chips.com>
2020-06-02 14:37:29 +0800

    RV1126/RV1109: BoardConfig-uvcc: default dts to rv1126-ai-cam-ddr3-v1.
    Change-Id: I078b1ad94d3fee54bdedbbc62d9bf92d5d0a255b
    Signed-off-by: huangjc <huangjc@rock-chips.com>

--- rv1126_rv1109/BoardConfig-uvcc.mk
+++ rv1126_rv1109/BoardConfig-uvcc.mk
@@ -11,7 +11,8 @@ export RK_KERNEL_DEFCONFIG=rv1126_defconfig
 # Kernel defconfig fragment
 export RK_KERNEL_DEFCONFIG_FRAGMENT=
 # Kernel dts
-export RK_KERNEL_DTS=rv1126-evb-ddr3-v10
+#export RK_KERNEL_DTS=rv1126-evb-ddr3-v10
+export RK_KERNEL_DTS=rv1126-ai-cam-ddr3-v1
 # boot image type
 export RK_BOOT_IMG=zboot.img
 # kernel image path
