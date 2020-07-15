467765b8b70da796e13f8b3dbcb9f7393dab1d2a
Yu YongZhen <yuyz@rock-chips.com>
2020-07-15 10:21:13 +0800

    oem_ipc: support multi resolution IPC products
    4K/2K/1080P
    
    Signed-off-by: Yu YongZhen <yuyz@rock-chips.com>
    Change-Id: Id66546a75dc06e6bb2e5e5babbdeb7e4c25351f5

--- oem/oem_ipc/RkLunch.sh
+++ oem/oem_ipc/RkLunch.sh
@@ -11,6 +11,21 @@ check_linker /userdata   /oem/www/userdata
 check_linker /media/usb0 /oem/www/usb0
 check_linker /mnt/sdcard /oem/www/sdcard
 
+if [ ! -f "/oem/sysconfig.db" ]; then
+  media-ctl -p -d /dev/media0 | grep 3840x2160
+  if [ $? -eq 0 ] ;then
+    ln -s -f /oem/sysconfig-4K.db /oem/sysconfig.db
+  fi
+  media-ctl -p -d /dev/media0 | grep 2688x1520
+  if [ $? -eq 0 ] ;then
+    ln -s -f /oem/sysconfig-2K.db /oem/sysconfig.db
+  fi
+  media-ctl -p -d /dev/media0 | grep 1920x1080
+  if [ $? -eq 0 ] ;then
+    ln -s -f /oem/sysconfig-1080P.db /oem/sysconfig.db
+  fi
+fi
+
 #set max socket buffer size to 1.5MByte
 sysctl -w net.core.wmem_max=1572864
 
