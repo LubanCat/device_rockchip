ceafeac34321698dd63a8ad9156ec78a415c1db5
Mark Huang <huangjc@rock-chips.com>
2020-08-24 18:02:27 +0800

    rv1126/rv1109: BoardConfig: add 128MB spi nand for uvcc
    Change-Id: I6b2a415d3aebe43a8ef58df269691a54a2e613a6
    Signed-off-by: Mark Huang <huangjc@rock-chips.com>

---
+++ rv1126_rv1109/BoardConfig-uvcc-spi-nand.mk
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Target chip
+export RK_CHIP=RV1126
+# Target arch
+export RK_ARCH=arm
+# Uboot defconfig
+export RK_UBOOT_DEFCONFIG=rv1126
+# Uboot image format type: fit(flattened image tree)
+export RK_UBOOT_FORMAT_TYPE=fit
+# Kernel defconfig
+export RK_KERNEL_DEFCONFIG=rv1126_defconfig
+# Kernel defconfig fragment
+export RK_KERNEL_DEFCONFIG_FRAGMENT=
+# Kernel dts
+#export RK_KERNEL_DTS=rv1126-evb-ddr3-v12-spi-nand
+export RK_KERNEL_DTS=rv1126-ai-cam-ddr3-v1-spi-nand
+# boot image type
+export RK_BOOT_IMG=zboot.img
+# kernel image path
+export RK_KERNEL_IMG=kernel/arch/arm/boot/zImage
+# parameter for GPT table
+export RK_PARAMETER=parameter-fit-128M.txt
+# Buildroot config
+export RK_CFG_BUILDROOT=rockchip_rv1126_rv1109_uvcc_spi_nand
+# Recovery config
+export RK_CFG_RECOVERY=
+# ramboot config
+export RK_CFG_RAMBOOT=
+# Pcba config
+export RK_CFG_PCBA=
+# Build jobs
+export RK_JOBS=12
+# target chip
+export RK_TARGET_PRODUCT=rv1126_rv1109
+# Set rootfs type, including ext2 ext4 squashfs ubi
+export RK_ROOTFS_TYPE=ubi
+# rootfs image path
+export RK_ROOTFS_IMG=rockdev/rootfs.${RK_ROOTFS_TYPE}
+# Set ramboot image type
+export RK_RAMBOOT_TYPE=
+# Set oem partition type, including ext2 squashfs
+export RK_OEM_FS_TYPE=
+# Set userdata partition type, including ext2, fat
+export RK_USERDATA_FS_TYPE=
+#OEM config
+export RK_OEM_DIR=
+#userdata config
+export RK_USERDATA_DIR=
+#misc image
+export RK_MISC=
+#choose enable distro module
+export RK_DISTRO_MODULE=
+# Define pre-build script for this board
+export RK_BOARD_PRE_BUILD_SCRIPT=app-build.sh
+# Define package-file for update.img
+export RK_PACKAGE_FILE=rv1126-package-file-spi-nand
