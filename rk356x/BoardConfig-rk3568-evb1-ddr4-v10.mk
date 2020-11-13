c438ab6b4c275069f2f49f0f6ea3c9196cd401ca
Hans Yang <yhx@rock-chips.com>
2020-11-13 14:16:27 +0800

    rk356x: BoardConfig.mk: add rockchip_linux_bifrost.config for kernel
    for linux platform bifrost can not compatible with mali config
    
    Usage:
    make ARCH=arm64 rockchip_linux_defconfig rockchip_linux_bifrost.config
    
    Signed-off-by: Hans Yang <yhx@rock-chips.com>
    Change-Id: I01244c12555471bba08309eeb2ef5f7803b6496d

--- rk356x/BoardConfig-rk3568-evb1-ddr4-v10.mk
+++ rk356x/BoardConfig-rk3568-evb1-ddr4-v10.mk
@@ -9,7 +9,7 @@ export RK_UBOOT_FORMAT_TYPE=fit
 # Kernel defconfig
 export RK_KERNEL_DEFCONFIG=rockchip_linux_defconfig
 # Kernel defconfig fragment
-export RK_KERNEL_DEFCONFIG_FRAGMENT=
+export RK_KERNEL_DEFCONFIG_FRAGMENT=rockchip_linux_bifrost.config
 # Kernel dts
 export RK_KERNEL_DTS=rk3568-evb1-ddr4-v10-linux
 # boot image type
