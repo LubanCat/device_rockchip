b0025713d5cd50a624221e102367ed943f4007a0
Mark Huang <huangjc@rock-chips.com>
2020-11-27 08:17:20 +0800

    oem_uvcc: Update RkApps-stop.sh to check usb net down
    Change-Id: Icf2dd450403162cf0e09c40ca4a90372c4859542
    Signed-off-by: Mark Huang <huangjc@rock-chips.com>

--- oem/oem_uvcc/RkApps-stop.sh
+++ oem/oem_uvcc/RkApps-stop.sh
@@ -2,11 +2,12 @@
 #
 killall -9 aicamera.sh
 killall smart_display_service
+ifconfig usb0 down
 killall dbserver
 killall aiserver
 #sleep for aiserver deint over
-sleep 1
-killall ispserver
+sleep .6
 killall uvc_app
+killall ispserver
 killall uac_app
 echo "All Stop Application ..."
