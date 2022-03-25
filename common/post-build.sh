e0900da49d04927bc1e63e38b38d3bde7e7bfce7
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-03-25 11:03:44 +0800

    post-build.sh: Fix comment about partition table variable
    <dev>:<mount point>:<fs type>:<mount flags>:<source dir>, for example:
    
    RK_EXTRA_PARTITIONS="oem userdata:/media:vfat:errors=remount-ro:userdata_empty"
    ->
    PARTLABEL=oem /oem auto defaults 0 2
    PARTLABEL=userdata /media vfat errors=remount-ro 0 2
    
    Change-Id: Ice941df2e7f6045ebfd349e9f15de300beca450a
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/post-build.sh
+++ common/post-build.sh
@@ -8,8 +8,8 @@ RK_LEGACY_PARTITIONS=" \
 	${RK_USERDATA_FS_TYPE:+userdata:/userdata:${RK_USERDATA_FS_TYPE}}
 "
 
-# <dev>:<mount point>:<fs type>:<source dir>, for example:
-# RK_EXTRA_PARTITIONS="oem:/oem:ext2:oem_normal userdata:/userdata:vfat:userdata_empty"
+# <dev>:<mount point>:<fs type>:<mount flags>:<source dir>, for example:
+# RK_EXTRA_PARTITIONS="oem:/oem:ext2:defaults:oem_normal userdata:/userdata:vfat:errors=remount-ro:userdata_empty"
 RK_EXTRA_PARTITIONS=${RK_EXTRA_PARTITIONS:-${RK_LEGACY_PARTITIONS}}
 
 function fixup_root()
