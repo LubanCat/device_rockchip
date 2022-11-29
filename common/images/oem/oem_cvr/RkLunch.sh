c37e6c5e2b785b81d2e3ab83872babea31108a2b
Zhichao Yu <zhichao.yu@rock-chips.com>
2022-11-29 20:39:47 +0800

    images/oem/cvr: make rkadk config compatible with imx335 and imx415
    Signed-off-by: Zhichao Yu <zhichao.yu@rock-chips.com>
    Change-Id: I62472401f3f4c839fab31b1ace275e2a5f79ccd6

--- common/images/oem/oem_cvr/RkLunch.sh
+++ common/images/oem/oem_cvr/RkLunch.sh
@@ -11,4 +11,13 @@ amixer -c 0 sset "Playback Path" SPK
 
 amixer -c 0 sset "Capture MIC Path" "Main Mic"
 
+media-ctl -p -d /dev/media1 | grep 3840x2160
+if [ $? -eq 0 ] ;then
+    ln -s -f /oem/etc/rkadk/8M/rkadk_setting_sensor_0.ini /oem/etc/rkadk/rkadk_setting_sensor_0.ini
+fi
+media-ctl -p -d /dev/media1 | grep 2592x1944
+if [ $? -eq 0 ] ;then
+    ln -s -f /oem/etc/rkadk/5M/rkadk_setting_sensor_0.ini /oem/etc/rkadk/rkadk_setting_sensor_0.ini
+fi
+
 LD_PRELOAD=/oem/libthird_media.so cvr_app &
