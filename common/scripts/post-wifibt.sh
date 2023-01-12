a1e3efed93e0cef8c898c3e7150222268bf7de49
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-01-12 17:40:05 +0800

    post-wifibt.sh: Ignore unused args
    mk-wifibt.sh doesn't like that.
    
    Change-Id: Ia137935c9bba946814f8586ce0729335ed58abeb
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/post-wifibt.sh
+++ common/scripts/post-wifibt.sh
@@ -4,4 +4,4 @@ source "${POST_HELPER:-$(dirname "$(realpath "$0")")/../post-hooks/post-helper}"
 
 echo "Building Wifi/BT module and firmwares..."
 
-"$SCRIPTS_DIR/mk-wifibt.sh" "$TARGET_DIR" $@
+"$SCRIPTS_DIR/mk-wifibt.sh" "$TARGET_DIR"
