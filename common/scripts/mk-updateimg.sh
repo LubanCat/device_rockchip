5a518cbd5f0ec19f8553354cc013edcd28da9a3b
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-02-20 17:08:41 +0800

    mk-updateimg.sh: Use generated package-file by default
    Set RK_PACKAGE_FILE or RK_OTA_PACKAGE_FILE to use custom package-file
    under <CHIP_DIR>/.
    
    Tested on RK3588 EVB:
    1/ Run ./build.sh updateimg
    2/ Check the generated output/update/package-file
    
    Change-Id: I4f69e2614dd1b4d4fbb56d500f19f0d755c36f85
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/mk-updateimg.sh
+++ common/scripts/mk-updateimg.sh
@@ -2,45 +2,93 @@
 
 RK_PACK_TOOL_DIR="$SDK_DIR/tools/linux/Linux_Pack_Firmware/rockdev"
 
+gen_package_file()
+{
+	TYPE="${1:-update}"
+
+	echo -e "# NAME\tPATH"
+	echo -e "package-file\tpackage-file"
+	echo -e "parameter\tparameter.txt"
+	echo -e "bootloader\tMiniLoaderAll.bin"
+
+	PARTITIONS="$(grep "^CMDLINE:" parameter.txt | \
+		grep -oE "\([^:^\)]*" | tr -d '(')"
+	for part in $PARTITIONS; do
+		if echo $part | grep -q "_b$"; then
+			# Not packing *_b partition for ota|sdcard
+			case $TYPE in
+				ota|sdcard) continue ;;
+			esac
+		fi
+
+		case $part in
+			backup) echo -e "backup\tRESERVED"; continue ;;
+			system|system_[ab]) IMAGE=rootfs.img ;;
+			*_a) IMAGE="${part%_a}.img" ;;
+			*_b) IMAGE="${part%_b}.img" ;;
+			*) IMAGE="$part.img" ;;
+		esac
+
+		[ ! -r "$IMAGE" ] || echo -e "$part\t$IMAGE"
+	done
+}
+
 build_updateimg()
 {
 	TARGET="${1:-$RK_FIRMWARE_DIR/update.img}"
-	OUT_DIR="${2:-$RK_OUTDIR/update}"
+	TYPE="${2:-update}"
 	PKG_FILE="${3:-$RK_PACKAGE_FILE}"
+	OUT_DIR="$RK_OUTDIR/$TYPE"
+	IMAGE_DIR="$OUT_DIR/Image"
 
-	if [ ! -r "$RK_PACK_TOOL_DIR/$PKG_FILE" ]; then
-		echo "$RK_PACK_TOOL_DIR/$PKG_FILE not exists!"
-		exit 1
+	rm -rf "$TARGET" "$OUT_DIR"
+	mkdir -p "$IMAGE_DIR"
+	cd "$IMAGE_DIR"
+
+	# Prepare images
+	ln -rsf "$RK_FIRMWARE_DIR"/* .
+	if [ "$TYPE" = sdcard ]; then
+		ln -rsf "$RK_IMAGE_DIR/sdupdate-ab-misc.img" misc.img
+		ln -rsf "$RK_IMAGE_DIR/parameter-sdupdate.txt" parameter.txt
+
+		# Not packing rootfs partition for sdcard
+		rm -f rootfs.img
 	fi
 
-	rm -rf "$OUT_DIR"
-	mkdir -p "$OUT_DIR"
+	# Prepare package-file
+	if [ "$PKG_FILE" ]; then
+		PKG_FILE="$CHIP_DIR/$PKG_FILE"
+		if [ ! -r "$PKG_FILE" ]; then
+			echo "$PKG_FILE not exists!"
+			exit 1
+		fi
+		ln -rsf "$PKG_FILE" package-file
+	else
+		echo "Generating package-file for $TYPE"
+		gen_package_file $TYPE > package-file
+	fi
 
-	ln -rsf "$RK_FIRMWARE_DIR" "$OUT_DIR/Image"
-	ln -rsf "$RK_PACK_TOOL_DIR/$PKG_FILE" "$OUT_DIR/package-file"
+	echo "Packing $TARGET for $TYPE..."
 
-	LOADER="$RK_FIRMWARE_DIR/MiniLoaderAll.bin"
-	TAG=RK$(hexdump -s 21 -n 4 -e '4 "%c"' $LOADER | rev)
-
-	echo "Packing $TARGET for $TAG..."
-
-	cd "$OUT_DIR"
+	TAG=RK$(hexdump -s 21 -n 4 -e '4 "%c"' MiniLoaderAll.bin | rev)
 	"$RK_PACK_TOOL_DIR/afptool" -pack ./ update.raw.img
-	"$RK_PACK_TOOL_DIR/rkImageMaker" -$TAG $LOADER update.raw.img \
-		update.img -os_type:androidos
+	"$RK_PACK_TOOL_DIR/rkImageMaker" -$TAG MiniLoaderAll.bin \
+		update.raw.img update.img -os_type:androidos
 
-	ln -rsf "$OUT_DIR/update.img" "$TARGET"
+	ln -rsf "$IMAGE_DIR/package-file" "$OUT_DIR"
+	ln -rsf "$IMAGE_DIR/update.img" "$OUT_DIR"
+	ln -rsf "$IMAGE_DIR/update.img" "$TARGET"
 
 	finish_build build_updateimg $@
 }
 
 build_ota_updateimg()
 {
-	check_config RK_AB_UPDATE RK_OTA_PACKAGE_FILE || return 0
+	check_config RK_AB_UPDATE || return 0
 
 	echo "Make A/B update image for OTA"
 
-	build_updateimg "$RK_FIRMWARE_DIR/update_ota.img" "$RK_OUTDIR/ota" \
+	build_updateimg "$RK_FIRMWARE_DIR/update_ota.img" ota \
 		$RK_OTA_PACKAGE_FILE
 
 	finish_build
@@ -52,17 +100,7 @@ build_sdcard_updateimg()
 
 	echo "Make A/B update image for SDcard"
 
-	for f in "$RK_IMAGE_DIR/sdupdate-ab-misc.img" \
-		"$RK_IMAGE_DIR/parameter-sdupdate.txt" \
-		"$RK_PACK_TOOL_DIR/sdcard-update-package-file" \
-		"$RK_FIRMWARE_DIR/recovery.img"; do
-		[ ! -r $f ] || continue
-		echo "$f not exists!"
-		return 1
-	done
-
-	build_updateimg "$RK_FIRMWARE_DIR/update_sdcard.img" "$RK_OUTDIR/sdcard" \
-		sdcard-update-package-file
+	build_updateimg "$RK_FIRMWARE_DIR/update_sdcard.img" sdcard
 
 	finish_build
 }
@@ -76,7 +114,7 @@ build_ab_updateimg()
 
 	echo "Make A/B update image"
 
-	build_updateimg "$RK_FIRMWARE_DIR/update_ab.img" "$RK_OUTDIR/ab"
+	build_updateimg "$RK_FIRMWARE_DIR/update_ab.img" ab
 
 	finish_build
 }
