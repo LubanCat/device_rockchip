3c6d89a19b7c83cca8f0337b0665e69c76e38296
hejiawencc <hjw0415@outlook.com>
2023-03-31 09:14:16 +0800

    rk356x 修复ubuntu桌面镜像为desktop

--- rk356x/BoardConfig-LubanCat-RK_series-ubuntu-desktop.mk
+++ rk356x/BoardConfig-LubanCat-RK_series-ubuntu-desktop.mk
@@ -68,7 +68,7 @@ export RK_ROOTFS_SYSTEM=ubuntu
 # 默认Ubuntu 版本
 export RK_UBUNTU_VERSION=20.04
 # 定义默认rootfs是否为桌面版  desktop :桌面版 	lite ：控制台版
-export RK_ROOTFS_TARGET=xfce
+export RK_ROOTFS_TARGET=desktop
 # 定义默认rootfs是否添加DEBUG工具  debug :添加 	none :不添加
 export RK_ROOTFS_DEBUG=none
 # 使用exboot内核分区
