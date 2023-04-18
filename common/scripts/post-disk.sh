9d336c8f59bb917d9ecf2fa3392e237ef097068a
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-04-18 11:03:40 +0800

    common: Support mountall and resizeall
    Tested with:
    ./build.sh post-rootfs debian/binary/
    
    Change-Id: If95f78cb3edb3bc1113a465e7b9ec58f90abb0db
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ common/scripts/post-disk.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+
+source "${POST_HELPER:-$(dirname "$(realpath "$0")")/../post-hooks/post-helper}"
+
+[ -z "$POST_RECOVERY" ] || exit 0
+[ -n "$RK_RESIZEALL" -o -n "$RK_MOUNTALL" ] || exit 0
+
+install_service()
+{
+	TYPE=$1
+
+	echo "Installing $TYPE service..."
+
+	install -m 0755 external/rkscript/$TYPE-helper \
+		"$TARGET_DIR/usr/bin/"
+
+	if [ -f "$TARGET_DIR/etc/init.d/rcS" ]; then
+		install -m 0755 external/rkscript/S21${TYPE}all.sh \
+			"$TARGET_DIR/etc/init.d/"
+	fi
+
+	[ "$TYPE" = resize ] || return 0
+
+	if [ -d "$TARGET_DIR/lib/systemd/system/" ]; then
+		install -m 0755 external/rkscript/$TYPE-all.service \
+			"$TARGET_DIR/lib/systemd/system/"
+		mkdir -p "$TARGET_DIR/etc/systemd/system/sysinit.target.wants"
+		ln -sf /lib/systemd/system/$TYPE-all.service \
+			"$TARGET_DIR/etc/systemd/system/sysinit.target.wants/"
+	fi
+
+	if [ -d "$TARGET_DIR/etc/rcS.d" ]; then
+		install -m 0755 external/rkscript/S21${TYPE}all.sh \
+			"$TARGET_DIR/etc/init.d/${TYPE}all.sh"
+		ln -sf ../init.d/${TYPE}all.sh \
+			"$TARGET_DIR/etc/rcS.d/S04${TYPE}all.sh"
+	fi
+}
+
+cd "$SDK_DIR"
+
+mkdir -p "$TARGET_DIR/etc" "$TARGET_DIR/lib" \
+	"$TARGET_DIR/usr/bin" "$TARGET_DIR/usr/lib"
+
+install -m 0755 external/rkscript/disk-helper "$TARGET_DIR/usr/bin/"
+
+[ -z "$RK_RESIZEALL" ] || install_service resize
+[ -z "$RK_MOUNTALL" ] || install_service mount
