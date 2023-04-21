910061f37f0893d9e63c7b889efe96e10e374504
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-04-21 12:32:03 +0800

    check-buildroot.sh: Check unbuffer
    Change-Id: I6ef8826bcb12574ed25840655615b575c4e54a93
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/check-buildroot.sh
+++ common/scripts/check-buildroot.sh
@@ -4,6 +4,14 @@ SCRIPTS_DIR="${SCRIPTS_DIR:-$(dirname "$(realpath "$0")")}"
 SDK_DIR="${SDK_DIR:-$SCRIPTS_DIR/../../../..}"
 BUILDROOT_DIR="$SDK_DIR/buildroot"
 
+# Buildroot brmake needs unbuffer
+if ! which unbuffer >/dev/null 2>&1; then
+	echo -e "\e[35mYour unbuffer is missing\e[0m"
+	echo "Please install it:"
+	echo "sudo apt-get install expect expect-dev"
+	exit 1
+fi
+
 # The new buildroot Makefile needs make (>= 4.0)
 if "$BUILDROOT_DIR/support/dependencies/check-host-make.sh" 4.0 make >/dev/null; then
 	exit 0
