b981872e9c872ca9b09c8d269d3b083cff5cbefd
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-04-24 15:48:18 +0800

    overlay: Support custom install script
    Execute "<overlay dir>/install.sh <rootfs dir> <OS type>" when exists.
    
    Change-Id: I0c9939936488568f88fad5c3e69b732fe1895530
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/post-overlay.sh
+++ common/scripts/post-overlay.sh
@@ -4,8 +4,16 @@ source "${POST_HELPER:-$(dirname "$(realpath "$0")")/../post-hooks/post-helper}"
 
 cd "$SDK_DIR"
 
-for overlay in "$COMMON_DIR/overlay-$POST_OS" $RK_ROOTFS_OVERLAY_DIRS; do
+for overlay in "$COMMON_DIR/overlays/overlay-$POST_OS" \
+	$RK_ROOTFS_OVERLAY_DIRS; do
 	[ -d "$overlay" ] || continue
+
+	if [ -x "$overlay/install.sh" ]; then
+		echo "Handling overlay: $overlay..."
+		"$overlay/install.sh" "$TARGET_DIR" "$POST_OS"
+		continue
+	fi
+
 	echo "Installing overlay: $overlay to $TARGET_DIR..."
 	rsync -av --chmod=u=rwX,go=rX "$overlay/" "$TARGET_DIR/"
 done
