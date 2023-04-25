f9456e69266a619de4e0e57c2defda166a805948
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-04-25 15:13:29 +0800

    check-power-domain.sh: Use RK_KERNEL_DTB for dtb directly
    Change-Id: Ib502210e69dbe8ef76b9fbdba640680a3349ee33
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/check-power-domain.sh
+++ common/scripts/check-power-domain.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-DTS=${RK_KERNEL_DTS%\.dts}
 dump_kernel_dtb_file=`mktemp`
 tmp_phandle_file=`mktemp`
 tmp_io_domain_file=`mktemp`
@@ -8,7 +7,7 @@ tmp_regulator_microvolt_file=`mktemp`
 tmp_final_target=`mktemp`
 tmp_grep_file=`mktemp`
 
-dtc -q -I dtb -O dts -o ${dump_kernel_dtb_file} $DTS.dtb
+dtc -q -I dtb -O dts -o ${dump_kernel_dtb_file} "$RK_KERNEL_DTB"
 
 if [ "$RK_SECURITY_CHECK_METHOD" = "DM-E" ] ; then
 	if ! grep -q "compatible = \"linaro,optee-tz\";" $dump_kernel_dtb_file; then
@@ -24,7 +23,7 @@ if [ "$RK_SECURITY_CHECK_METHOD" = "DM-E" ] ; then
 fi
 
 if ! grep -Pzo "io-domains\s*{(\n|\w|-|;|=|<|>|\"|_|\s|,)*};" $dump_kernel_dtb_file 1>$tmp_grep_file 2>/dev/null; then
-	echo "Not Found io-domains in $DTS.dts"
+	echo "Not Found io-domains in $RK_KERNEL_DTS"
 	rm -f $tmp_grep_file
 	exit 0
 fi
@@ -57,9 +56,9 @@ done < $tmp_regulator_microvolt_file
 
 echo -e "\e[41;1;30m PLEASE CHECK BOARD GPIO POWER DOMAIN CONFIGURATION !!!!!\e[0m"
 echo -e "\e[41;1;30m <<< ESPECIALLY Wi-Fi/Flash/Ethernet IO power domain >>> !!!!!\e[0m"
-echo -e "\e[41;1;30m Check Node [pmu_io_domains] in the file: $DTS.dts \e[0m"
+echo -e "\e[41;1;30m Check Node [pmu_io_domains] in the file: $RK_KERNEL_DTS \e[0m"
 echo
 echo -e "\e[41;1;30m 请再次确认板级的电源域配置！！！！！！\e[0m"
 echo -e "\e[41;1;30m <<< 特别是Wi-Fi，FLASH，以太网这几路IO电源的配置 >>> ！！！！！\e[0m"
-echo -e "\e[41;1;30m 检查内核文件 $DTS.dts 的节点 [pmu_io_domains] \e[0m"
+echo -e "\e[41;1;30m 检查内核文件 $RK_KERNEL_DTS 的节点 [pmu_io_domains] \e[0m"
 cat $tmp_final_target
