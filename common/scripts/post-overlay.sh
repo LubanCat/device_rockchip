d3906b49ac9ed4ad25329bc5a08ea5aaace52e19
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-05-05 11:07:25 +0800

    post-overlay.sh: Avoid installing extra overlays to recovery and pcba
    Change-Id: I88f717deb8fbf69e7e0793958952fa0a97d40a2a
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/post-overlay.sh
+++ common/scripts/post-overlay.sh
@@ -2,18 +2,30 @@
 
 source "${POST_HELPER:-$(dirname "$(realpath "$0")")/../post-hooks/post-helper}"
 
+install_overlay()
+{
+	OVERLAY="$1"
+
+	[ -d "$OVERLAY" ] || return 0
+
+	if [ -x "$OVERLAY/install.sh" ]; then
+		echo "Handling overlay: $OVERLAY..."
+		"$OVERLAY/install.sh" "$TARGET_DIR" "$POST_OS"
+	else
+		echo "Installing overlay: $OVERLAY to $TARGET_DIR..."
+		rsync -av --chmod=u=rwX,go=rX "$OVERLAY/" "$TARGET_DIR/"
+	fi
+}
+
 cd "$SDK_DIR"
 
-for overlay in "$COMMON_DIR/overlays/overlay-$POST_OS" \
-	"$COMMON_DIR/overlays/overlay-fonts" $RK_ROOTFS_OVERLAY_DIRS; do
-	[ -d "$overlay" ] || continue
+install_overlay "$COMMON_DIR/overlays/overlay-$POST_OS"
 
-	if [ -x "$overlay/install.sh" ]; then
-		echo "Handling overlay: $overlay..."
-		"$overlay/install.sh" "$TARGET_DIR" "$POST_OS"
-		continue
-	fi
+# No extra overlays for recovery and pcba
+case "$POST_OS" in
+	recovery | pcba) exit 0 ;;
+esac
 
-	echo "Installing overlay: $overlay to $TARGET_DIR..."
-	rsync -av --chmod=u=rwX,go=rX "$overlay/" "$TARGET_DIR/"
+for overlay in "$COMMON_DIR/overlays/overlay-fonts" $RK_ROOTFS_OVERLAY_DIRS; do
+	install_overlay "$overlay"
 done
