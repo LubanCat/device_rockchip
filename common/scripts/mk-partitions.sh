d8015e97c51af053a0ad4656e6043d3464fd05e1
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-05-19 15:26:00 +0800

    scripts: Support interactive modifying partitions
    Tested with:
    make mod-parts
    
    Change-Id: Ib15f308e2883981646226d797fbdba641b901fd9
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/mk-partitions.sh
+++ common/scripts/mk-partitions.sh
@@ -1,8 +1,46 @@
 #!/bin/bash -e
 
-print_parameter()
+modify_partitions()
 {
-	return 0
+	echo "=========================================="
+	echo "          Start modifying partitions"
+	echo "=========================================="
+
+	rk_partition_print
+	echo
+	echo "Usage:"
+	core_usage | grep -v "^mod-parts"
+
+	while true; do
+		echo
+		read -p "Commands (? for help): " SUB_CMD ARGS || break
+		case "${SUB_CMD:-print-parts}" in
+			"done") break ;;
+			print-parts)
+				rk_partition_print
+				continue
+				;;
+			edit-parts) FUNC=rk_partition_edit ;;
+			new-parts) FUNC=rk_partition_create ;;
+			insert-part) FUNC=rk_partition_insert ;;
+			del-part) FUNC=rk_partition_del ;;
+			move-part) FUNC=rk_partition_move ;;
+			rename-part) FUNC=rk_partition_rename ;;
+			resize-part) FUNC=rk_partition_resize ;;
+			help | h | -h | --help | \?) FUNC=false ;;
+			*)
+				echo "Unknown command: $SUB_CMD"
+				FUNC=false
+				;;
+		esac
+
+		if $FUNC $ARGS; then
+			rk_partition_print
+		else
+			core_usage | grep -v "^mod-parts"
+			echo -e "done                               \tdone modifying"
+		fi
+	done
 }
 
 # Hooks
@@ -10,6 +48,7 @@ print_parameter()
 usage_hook()
 {
 	echo -e "print-parts                        \tprint partitions"
+	echo -e "mod-parts                          \tinteractive partition table modify"
 	echo -e "edit-parts                         \tedit raw partitions"
 	echo -e "new-parts:<offset>:<name>:<size>...\tre-create partitions"
 	echo -e "insert-part:<idx>:<name>[:<size>]  \tinsert partition"
@@ -19,7 +58,7 @@ usage_hook()
 	echo -e "resize-part:(<idx>|<name>):<size>  \tresize partition"
 }
 
-PRE_BUILD_CMDS="print-parts edit-parts new-parts insert-part del-part move-part rename-part resize-part"
+PRE_BUILD_CMDS="print-parts mod-parts edit-parts new-parts insert-part del-part move-part rename-part resize-part"
 pre_build_hook()
 {
 	check_config RK_PARAMETER || return 0
@@ -29,6 +68,7 @@ pre_build_hook()
 
 	case "$CMD" in
 		print-parts) rk_partition_print $@ ;;
+		mod-parts) modify_partitions $@ ;;
 		edit-parts) rk_partition_edit $@ ;;
 		new-parts) rk_partition_create $@ ;;
 		insert-part) rk_partition_insert $@ ;;
@@ -36,6 +76,10 @@ pre_build_hook()
 		move-part) rk_partition_move $@ ;;
 		rename-part) rk_partition_rename $@ ;;
 		resize-part) rk_partition_resize $@ ;;
+		*)
+			core_usage
+			exit 1
+			;;
 	esac
 
 	finish_build $CMD $@
