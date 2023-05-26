765ff6bc96e956ce1394e155e1cf12b797bfcf5e
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-05-26 16:14:21 +0800

    data: tools: Select perf version at runtime
    Tested on RK3588 EVB with:
    1/ perf top&
    2/ ps | grep 5.10
    
    Change-Id: I3f8b9c2bfbb0d6f270888576f71edea1a7b524c8
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/data/tools/install.sh
+++ common/data/tools/install.sh
@@ -10,4 +10,3 @@ echo "Installing prebuilt tools: $OVERLAY_DIR to $TARGET_DIR..."
 mkdir -p "$TARGET_DIR/usr/local/bin/"
 rsync -av --chmod=u=rwX,go=rX --exclude=install.sh --exclude=adbd \
 	"$OVERLAY_DIR/" "$TARGET_DIR/usr/local/bin/"
-ln -sf perf-$RK_KERNEL_VERSION_REAL "$TARGET_DIR/usr/local/bin/perf"
