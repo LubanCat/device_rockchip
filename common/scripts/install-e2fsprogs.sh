678f2ebe7546cc4dcdbd86aaaceb296c36121bc5
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-06-15 17:54:09 +0800

    install-e2fsprogs.sh: Fix a typo
    Tested with:
    ./device/rockchip/common/scripts/install-e2fsprogs.sh | sh
    
    Change-Id: Ie4d1f630a09381156f2eb1106f43cf23d10b1a3c
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/install-e2fsprogs.sh
+++ common/scripts/install-e2fsprogs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # 1.46.6 has -d option and without metadata_csum_seed and orphan_file features
-E2FSPROGS_VER=1.46.6
+E2FSPROGS_VER="v1.46.6"
 echo "git clone https://git.kernel.org/pub/scm/fs/ext2/e2fsprogs.git --depth 1 -b $E2FSPROGS_VER"
 echo "cd e2fsprogs"
 echo "./configure"
-echo "sudo make install -j8"
+echo "sudo make install -j8 -k -i"
