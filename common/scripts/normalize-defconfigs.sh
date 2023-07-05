baf36f0b244c00f6bacc90d8535cf6b8aa60349f
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-07-05 17:05:29 +0800

    Normalize defconfigs
    Run device/rockchip/common/scripts/normalize-defconfigs.sh
    
    Change-Id: Ib7e080af073755d985b893a8aefaadc3c7d7b5c0
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/normalize-defconfigs.sh
+++ common/scripts/normalize-defconfigs.sh
@@ -5,8 +5,8 @@ SDK_DIR="${SDK_DIR:-$SCRIPTS_DIR/../../../..}"
 
 cd "$SDK_DIR"
 
-for c in $(find device/rockchip/.chips \
-	-type f -mindepth 1 -maxdepth 1 -name "rockchip_*_defconfig"); do
+for c in $(find device/rockchip/.chips/ \
+	-mindepth 2 -maxdepth 2 -type f -name "rockchip_*_defconfig"); do
 	./build.sh $c
 	make savedefconfig
 	sed -i '/RK_KERNEL_VERSION/d' $c
