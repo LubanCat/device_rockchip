adcf84417979526d312c0fa9615f35b1c825176c
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-08-01 10:32:53 +0800

    common: Require qemu-user-static >= 5 for debian
    The old qemu might crash when running ldconfig.
    
    Change-Id: I214122c87b68eafa67f392a792c469947ed9e878
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/check-debian.sh
+++ common/scripts/check-debian.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 SCRIPTS_DIR="${SCRIPTS_DIR:-$(dirname "$(realpath "$0")")}"
+RK_DEBIAN_ARCH="${RK_DEBIAN_ARCH:-arm64}"
+RK_DATA_DIR="${RK_DATA_DIR:-"$SCRIPTS_DIR/../data/"}"
 
 if [ ! -e "/usr/share/live/build/data/debian-cd/$RK_DEBIAN_VERSION" ]; then
 	echo -e "\e[35m"
@@ -22,6 +24,33 @@ if grep -wq metadata_csum_seed /etc/mke2fs.conf; then
 	echo "Your mke2fs is too new: $(mke2fs -V 2>&1 | head -n 1)"
 	echo "Please downgrade it:"
 	"$SCRIPTS_DIR/install-e2fsprogs.sh"
+	echo -e "\e[0m"
+	exit 1
+fi
+
+case "$RK_DEBIAN_ARCH" in
+	arm64) QEMU_ARCH=aarch64 ;;
+	armhf) QEMU_ARCH=arm ;;
+esac
+QEMU_VERSION=$(qemu-$QEMU_ARCH-static --version | head -n 1 | cut -d' ' -f3)
+
+if ! which qemu-$QEMU_ARCH-static >/dev/null 2>&1; then
+	echo -e "\e[35m"
+	echo "Your qemu-$QEMU_ARCH-static(qemu-user-static) is missing"
+	echo "Please install it:"
+	echo "sudo apt-get install qemu-user-static"
+	echo -e "\e[0m"
+	exit 1
+fi
+
+if [ ${QEMU_VERSION%%.*} -lt 5 ]; then
+	echo -e "\e[35m"
+	echo "Your qemu-$QEMU_ARCH-static is too old: $QEMU_VERSION"
+	echo "Please upgrade it:"
+	echo "sudo update-binfmts --unimport qemu-$QEMU_ARCH"
+	echo "sudo rm -f /usr/bin/qemu-$QEMU_ARCH-static"
+	echo "sudo cp $RK_DATA_DIR/qemu-$QEMU_ARCH-static /usr/bin/"
+	echo "sudo update-binfmts --import qemu-$QEMU_ARCH"
 	echo -e "\e[0m"
 	exit 1
 fi
