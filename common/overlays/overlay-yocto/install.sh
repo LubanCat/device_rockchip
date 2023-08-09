7d0be0b8377cb5c096ad57115d7ea2d02c0edb78
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-08-09 16:52:02 +0800

    overlay: yocto: Start weston with root user
    Tested on RK3588 EVB with:
    adb shell weston-simple-egl
    
    Change-Id: I7c72eaa4a338f015b780a0ec5b53f6f48a1fedbf
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/overlays/overlay-yocto/install.sh
+++ common/overlays/overlay-yocto/install.sh
@@ -6,7 +6,16 @@ TARGET_DIR="$1"
 OVERLAY_DIR="$(dirname "$(realpath "$0")")"
 SDK_DIR="${SDK_DIR:-$(realpath "$OVERLAY_DIR/../../../../..")}"
 
+# Login root on serial console
+sed -i 's~\(respawn:\)/bin/start_getty.*~\1/bin/login -p root~' \
+	"$TARGET_DIR/etc/inittab"
+
+# Use uid to detect root user
+sed -i 's~"$HOME" != "/home/root"~$(id -u) -ne 0~' "$TARGET_DIR/etc/profile"
+
 if [ -x "$TARGET_DIR/usr/bin/weston" ]; then
+	sed -i 's/\(WESTON_USER=\)weston/\1root/' "$TARGET_DIR/etc/init.d/weston"
+
 	echo "Installing weston overlay: $OVERLAY_DIR/weston to $TARGET_DIR..."
 	rsync -av --chmod=u=rwX,go=rX "$OVERLAY_DIR/weston/" "$TARGET_DIR/" \
 		--exclude="$(basename "$(realpath "$0")")"
