3649f686ff8dead2c265aead81fb0a92a388d449
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-09-14 18:38:11 +0800

    overlays: Add prebuild frecon
    Frecon is an userspace virtual terminal.
    
    Tested with:
    1/ make shell
    2/ mkdir /tmp/debian
    3/ $SCRIPTS_DIR/post-overlay.sh /tmp/debian debian systemd frecon
    
    Change-Id: I54c77f76282868de70028013b121aa63053b862f
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ common/overlays/rootfs/frecon/etc/profile.d/frecon.sh
@@ -0,0 +1,5 @@
+export FRECON_SHELL=bash
+export FRECON_VTS=1
+# export FRECON_VT1=1
+export FRECON_FB_ROTATE=0
+export FRECON_FB_SCALE=1
---
+++ common/overlays/rootfs/frecon/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+[ "$RK_ROOTFS_FRECON" ] || exit 0
+
+TARGET_DIR="$1"
+[ "$TARGET_DIR" ] || exit 1
+
+OVERLAY_DIR="$(dirname "$(realpath "$0")")"
+
+echo "Installing frecon to $TARGET_DIR..."
+$RK_RSYNC "$OVERLAY_DIR/usr" "$OVERLAY_DIR/etc" "$TARGET_DIR/"
+
+install_sysv_service "$OVERLAY_DIR/S35frecon" start 5 4 3 2 stop 0 1 6
+install_busybox_service "$OVERLAY_DIR/S35frecon"
+install_systemd_service "$OVERLAY_DIR/frecon.service" \
+	"$OVERLAY_DIR/S35frecon" "/etc/init.d/frecon"
