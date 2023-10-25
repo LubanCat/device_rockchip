5696a51bcdd472e47f10fa34465040ab0f074a79
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-10-25 18:48:40 +0800

    overlays: frecon: Add more configs
    Change-Id: Ic13243a40c4f495fa0b13e21700cc84e1556e49e
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/overlays/rootfs/frecon/install.sh
+++ common/overlays/rootfs/frecon/install.sh
@@ -10,6 +10,15 @@ OVERLAY_DIR="$(dirname "$(realpath "$0")")"
 echo "Installing frecon to $TARGET_DIR..."
 $RK_RSYNC "$OVERLAY_DIR/usr" "$OVERLAY_DIR/etc" "$TARGET_DIR/"
 
+mkdir -p "$TARGET_DIR/etc/profile.d"
+{
+	echo "export FRECON_SHELL=$RK_ROOTFS_FRECON_SHELL"
+	[ -z "$RK_ROOTFS_FRECON_VTS" ] || echo "export FRECON_VTS=1"
+	[ -z "$RK_ROOTFS_FRECON_VT1" ] || echo "export FRECON_VT1=1"
+	echo "export FRECON_FB_ROTATE=$RK_ROOTFS_FRECON_ROTATE"
+	echo "export FRECON_FB_SCALE=$RK_ROOTFS_FRECON_SCALE"
+} > "$TARGET_DIR/etc/profile.d/frecon.sh"
+
 install_sysv_service "$OVERLAY_DIR/S35frecon" 5 4 3 2 K02 0 1 6
 install_busybox_service "$OVERLAY_DIR/S35frecon"
 install_systemd_service "$OVERLAY_DIR/frecon.service" \
