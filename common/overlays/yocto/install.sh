c9daa1a2c0c461b20ee2a5547dd94e1f035691da
Jiajian Wu <jair.wu@rock-chips.com>
2023-11-01 15:30:53 +0800

    overlays: Add pulseaudio configurations for yocto
    Change-Id: Ifc6a36e52637717ffc98d54e1170f4f88dae41b5
    Signed-off-by: Jiajian Wu <jair.wu@rock-chips.com>

--- common/overlays/yocto/install.sh
+++ common/overlays/yocto/install.sh
@@ -49,3 +49,12 @@ if [ "$RK_YOCTO_USBMOUNT" ]; then
 		ln -sf "/mnt/$type" "$TARGET_DIR/media/${type}0"
 	done
 fi
+
+# Install pulseaudio
+if [ -x "$TARGET_DIR/usr/bin/pulseaudio" ]; then
+	$RK_RSYNC "$TARGET_DIR/etc/pulse" "$TARGET_DIR/etc/"
+
+	mkdir -p "$TARGET_DIR/etc/rcS.d"
+	install -m 0755 "$OVERLAY_DIR/pulseaudio/etc/rcS.d/S50pulseaudio" \
+		"$TARGET_DIR/etc/rcS.d/"
+fi
