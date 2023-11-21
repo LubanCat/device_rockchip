dfee768d1c6144d36331347383c0c5a3f60f2f19
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-11-21 09:36:43 +0800

    bootanim: Update to newest version
    Change-Id: I9609ea0776ad857b09bb9086c604e42e994492ae
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/overlays/rootfs/bootanim/install.sh
+++ common/overlays/rootfs/bootanim/install.sh
@@ -18,7 +18,8 @@ sed -i "s/^\(TIMEOUT=\).*/\1$RK_ROOTFS_BOOTANIM_TIMEOUT/" \
 
 install_sysv_service external/rkscript/S*bootanim.sh S
 install_busybox_service external/rkscript/S*bootanim.sh
-install_systemd_service external/rkscript/bootanim.service
+install_systemd_service external/rkscript/bootanim.service \
+	external/rkscript/S31bootanim.sh /etc/init.d/bootanim
 
 rm -rf "$TARGET_DIR/etc/bootanim.d"
 $RK_RSYNC "$OVERLAY_DIR/etc" "$TARGET_DIR/"
