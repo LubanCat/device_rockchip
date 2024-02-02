489f6327785a8a9f5c4cc6ef81fb10e6ee8d2fce
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-02-02 10:29:43 +0800

    check-buildroot.sh: Require host linux at least 4.15
    Change-Id: Ie15e7ee52dc74b1a13efad6966e7e4046f0b8468
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/check-buildroot.sh
+++ common/scripts/check-buildroot.sh
@@ -4,6 +4,28 @@ RK_SCRIPTS_DIR="${RK_SCRIPTS_DIR:-$(dirname "$(realpath "$0")")}"
 RK_SDK_DIR="${RK_SDK_DIR:-$RK_SCRIPTS_DIR/../../../..}"
 BUILDROOT_DIR="$RK_SDK_DIR/buildroot"
 
+# Check for host linux version
+LINUX_VER_MAJOR=$(uname -r | cut -d'.' -f1)
+LINUX_VER_MINOR=$(uname -r | cut -d'.' -f2)
+if [ "$LINUX_VER_MAJOR" -lt 4 ] ||
+	[ "$LINUX_VER_MAJOR" -eq 4 -a "$LINUX_VER_MINOR" -lt 15 ]; then
+	echo -e "\e[35m"
+	echo "Your host linux version is too old: $(uname -r)"
+	echo "Please upgrade it to at least 4.15!"
+	echo -e "\e[0m"
+	exit 1
+fi
+
+# Buildroot brmake needs unbuffer
+if ! which unbuffer >/dev/null 2>&1; then
+	echo -e "\e[35m"
+	echo "Your unbuffer is missing"
+	echo "Please install it:"
+	echo "sudo apt-get install expect expect-dev"
+	echo -e "\e[0m"
+	exit 1
+fi
+
 # Buildroot brmake needs unbuffer
 if ! which unbuffer >/dev/null 2>&1; then
 	echo -e "\e[35m"
