bb8dd4333ebd266757bf7d9d30ab1c34c342cd61
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-02-23 18:00:24 +0800

    post-rootfs: Add log-guardian
    Truncate log files when no space left on device.
    
    Change-Id: I0e13b2432676b59d887faadcbc271588d41f88ce
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ common/scripts/post-log-guardian.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+POST_ROOTFS_ONLY=1
+
+source "${RK_POST_HELPER:-$(dirname "$(realpath "$0")")/../post-hooks/post-helper}"
+
+[ "$RK_ROOTFS_LOG_GUARDIAN" ] || exit 0
+
+message "Installing log-guardian..."
+
+cd "$RK_SDK_DIR"
+
+mkdir -p "$TARGET_DIR/usr/bin"
+install -m 0755 external/rkscript/log-guardian "$TARGET_DIR/usr/bin/"
+
+sed -i -e "s/^\(INTERVAL=\).*/\1\"$RK_ROOTFS_LOG_GUARDIAN_INTERVAL\"/" \
+	-e "s/^\(MIN_AVAIL_SIZE=\).*/\1\"$RK_ROOTFS_LOG_GUARDIAN_MIN_SIZE\"/" \
+	-e "s#^\(LOG_DIRS=\).*#\1\"$RK_ROOTFS_LOG_GUARDIAN_LOG_DIRS\"#" \
+        "$TARGET_DIR/usr/bin/log-guardian"
+
+install_sysv_service external/rkscript/S*log-guardian.sh S
+install_busybox_service external/rkscript/S*log-guardian.sh
+install_systemd_service external/rkscript/log-guardian.service
