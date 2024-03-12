fdd71b34edaa196bcf911f0861f51949ba4bf342
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-03-12 11:22:34 +0800

    overlays: generate_logs: Fix wrong dumped DRM file names
    Fix a typo.
    
    Change-Id: I740ca42570885108f1c285203c68ba87e308c9b7
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/overlays/rootfs/generate-logs/etc/generate_logs.d/20-drm.sh
+++ common/overlays/rootfs/generate-logs/etc/generate_logs.d/20-drm.sh
@@ -8,6 +8,6 @@ modetest -a > modetest.txt
 type kmsgrab >/dev/null 2>/dev/null || exit 0
 for p in $(modetest -p | grep -A 1000 "^P" | grep -o "^[0-9]*"); do
 	DUMP=plane_$p
-	kmsgrab --plane $p >$DUMP.txt 2>$DUMP.raw
+	kmsgrab --plane $p >$DUMP.raw 2>$DUMP.txt
 	[ "$(wc -c $DUMP.txt | cut -d' ' -f1)" -ne 0 ] || rm -rf $DUMP.*
 done
