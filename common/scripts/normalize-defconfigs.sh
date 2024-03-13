979c052330a583804e2847d1762b91da9dd544da
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-03-13 11:32:25 +0800

    normalize-defconfigs.sh: Support normalizing all defconfigs
    Tested with:
    ./device/rockchip/common/scripts/normalize-defconfigs.sh
    
    Change-Id: Ie340ee7c70f11a0345f4e5e6735b4fe66d8175a9
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/normalize-defconfigs.sh
+++ common/scripts/normalize-defconfigs.sh
@@ -5,11 +5,13 @@ RK_SDK_DIR="${RK_SDK_DIR:-$RK_SCRIPTS_DIR/../../../..}"
 
 cd "$RK_SDK_DIR"
 
-for chip in ${@:-""}; do
-	for c in $(find "device/rockchip/.chips/$(basename "$chip")" \
+CHIPS="${@:-$(ls device/rockchip/.chips/)}"
+
+for chip in $CHIPS; do
+	for cfg in $(find "device/rockchip/.chips/$(basename "$chip")" \
 		-mindepth 1 -maxdepth 2 -type f \
 		-name "rockchip_*_defconfig"); do
-		./build.sh $c
+		./build.sh $chip:$(basename $cfg)
 		make savedefconfig
 	done
 done
