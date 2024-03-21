daecf5e9c27fb253be720f4ebedcfb6ad5e3b30b
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-03-21 10:35:34 +0800

    common: Support creating glibc ld.so.cache
    Tested with:
    1/ rm -f output/buildroot/target/etc/ld.so.cache
    2/ make shell
    3/ $RK_SCRIPTS_DIR/post-ldcache.sh output/buildroot/target
    
    Change-Id: Ic28cb15886ee6525a65065d8e3472ceb6b6a6f61
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ common/scripts/post-ldcache.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+source "${RK_POST_HELPER:-$(dirname "$(realpath "$0")")/../post-hooks/post-helper}"
+
+[ "$RK_ROOTFS_LD_CACHE" ] || exit 0
+
+if [ -r "$TARGET_DIR/etc/ld.so.cache" ]; then
+	notice "Keep original /etc/ld.so.cache"
+	exit 0
+fi
+
+if ! grep -q glibc-ld.so.cache "$TARGET_DIR"/lib/ld-linux* &>/dev/null; then
+	notice "glibc's ld.so.cache is unsupported"
+	exit 0
+fi
+
+message "Creating ld.so.cache for $TARGET ..."
+
+"$RK_DATA_DIR/ldconfig/ldconfig" -r "$TARGET_DIR/"
