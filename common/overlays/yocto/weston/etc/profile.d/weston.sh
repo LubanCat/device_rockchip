22e131dec70f4945adee294ebb9aa5a446aaf7dd
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-04-17 16:26:49 +0800

    overlays: yocto: weston: Update env file
    Change-Id: I7cf4e741afff6de04b633d1dd62e5a5446e1ff31
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/overlays/yocto/weston/etc/profile.d/weston.sh
+++ common/overlays/yocto/weston/etc/profile.d/weston.sh
@@ -1,7 +1,14 @@
 # The env variables below can be overridden
 
+# HACK for old chromium, see:
+# https://bugs.chromium.org/p/chromium/issues/detail?id=1279574
+export WL_OUTPUT_VERSION=3
+
 # Comment out this for atomic related functions, e.g. sprites
 export WESTON_DISABLE_ATOMIC=1
+
+# Increasing this can help to reduce tearing when dumping low-level DRM buffers
+export WESTON_DRM_MIN_BUFFERS=2
 
 # Allow using drm modifier, e.g. ARM AFBC
 # export WESTON_ALLOW_GBM_MODIFIERS=1
@@ -9,10 +16,17 @@ export WESTON_DISABLE_ATOMIC=1
 # Enable black background for fullscreen views
 # export WESTON_FULLSCREEN_BLACK_BACKGROUND=1
 
+# Set default layer for the first APP
+# export WESTON_FIRST_APP_LAYER=top
+# export WESTON_FIRST_APP_LAYER=bottom
+
 # Allow disabling unused CRTCs
 # WESTON_DRM_MASTER=1
 
-# Override output's freezing time
+# Override initial freezing time
+# export WESTON_DRM_INITIAL_FREEZE_MS=100
+
+# Override output's resized freezing time
 # export WESTON_DRM_RESIZE_FREEZE_MS=1000
 
 # Primary screen
@@ -45,17 +59,29 @@ export WESTON_DRM_KEEP_RATIO=1
 # Tag file for freezing weston display
 export WESTON_FREEZE_DISPLAY=/tmp/.freeze_weston
 
+# Try to pin views to the assigned output
+# export WESTON_OUTPUT_PIN=1
+
 # Set dynamic config file path
 # export WESTON_DRM_CONFIG=/tmp/.weston_drm.conf
 #
 # Dynamic config examples:
-# echo "compositor:state:sleep" > /tmp/.weston_drm.conf # off + input wakeable
+# echo "compositor:state:offscreen" > /tmp/.weston_drm.conf # off + input
+# echo "compositor:state:sleep" > /tmp/.weston_drm.conf # off + input(wakeable)
 # echo "compositor:state:block" > /tmp/.weston_drm.conf # no input
 # echo "compositor:state:freeze" > /tmp/.weston_drm.conf # no input + freeze
 # echo "compositor:state:off" > /tmp/.weston_drm.conf # no input + off
 # echo "compositor:state:on" > /tmp/.weston_drm.conf
+# echo "compositor:hotplug:off" > /tmp/.weston_drm.conf
+# echo "compositor:hotplug:on" > /tmp/.weston_drm.conf
+# echo "compositor:hotplug:force" > /tmp/.weston_drm.conf
+# echo "compositor:cursor:hide" > /tmp/.weston_drm.conf
+# echo "compositor:cursor:show" > /tmp/.weston_drm.conf
+# echo "compositor:output:pin" > /tmp/.weston_drm.conf
+# echo "compositor:output:unpin" > /tmp/.weston_drm.conf
 # echo "output:DSI-1:off" > /tmp/.weston_drm.conf
 # echo "output:eDP-1:freeze" > /tmp/.weston_drm.conf
+# echo "output:DSI-1:offscreen" > /tmp/.weston_drm.conf
 # echo "output:DSI-1:on" > /tmp/.weston_drm.conf
 # echo "output:all:rotate90" > /tmp/.weston_drm.conf
 # echo "output:all:rect=<100,20,1636,2068>" > /tmp/.weston_drm.conf
