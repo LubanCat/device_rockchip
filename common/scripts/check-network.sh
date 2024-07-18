1c90f23efeaeb93b04728e5e97ee9ce0374f2d60
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-07-18 16:40:28 +0800

    check-network.sh: Allow bypass network checking
    Unset RK_NETWORK_CHECK to ignore network checking.
    
    Change-Id: I7afbed83edb7168994c18113068fa3c349c86a0e
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- common/scripts/check-network.sh
+++ common/scripts/check-network.sh
@@ -11,5 +11,19 @@ fi
 echo -e "\e[35m"
 echo -e "Your network is not able to access $SITE_NAME!"
 echo -e "$EXTRA_MSG"
-echo -e "\e[0m"
-exit 1
+echo -ne "\e[0m"
+
+if [ -z "$RK_NETWORK_CHECK" ]; then
+	echo -ne "\e[35m"
+	echo "Will continue in 5 seconds ..."
+	for S in $(seq 0 4); do
+		echo "$((5 - $S)) ..."
+		sleep 1
+	done
+	echo -e "\e[0m"
+else
+	echo -ne "\e[35m"
+	echo "Unset RK_NETWORK_CHECK in the SDK config to continue..."
+	echo -e "\e[0m"
+	exit 1
+fi
